81238777d0d56d331e1f1216392045a8cc3fa3f1
unknown <Trent@.(none)>
2010-08-13 22:41:15 +1200

    initial version of uniform.like.Rd

--- distance/R/F.nLL.r
+++ distance/R/F.nLL.r
@@ -1,11 +1,11 @@
 F.nLL <- function(a, dist, like, w.lo=0, w.hi=max(dist), series, expansions=0){
 
-    f.loglike <- match.fun(paste( like, ".like", sep=""))
+    f.like <- match.fun(paste( like, ".like", sep=""))
 
-    LL <- f.loglike( a, dist, w.lo=w.lo, w.hi=w.hi, series, expansions )
+    L <- f.like( a, dist, w.lo=w.lo, w.hi=w.hi, series, expansions )
 
-    LL[ !is.na(LL) & (LL <= 0) ] <- 1e-6   # happens at very bad values of parameters
+    L[ !is.na(L) & (L <= 0) ] <- 1e-6   # happens at very bad values of parameters
 
-    nLL <- -sum(log(LL), na.rm=T)  # Note that distances > w in LL are set to NA
+    nLL <- -sum(log(L), na.rm=T)  # Note that distances > w in LL are set to NA
     nLL
 }
