bf41b166db3e7f7462057203122c44c17f5ac549
Jason Carlisle <jason.d.carlisle@gmail.com>
2015-02-02 11:31:28 -0700

    vignette.example updates 2/2/2015
    updated example to use example data, fit detection function, and
    estimate abundance.  Some issues with NAs in group sizes.  The functions
    edited by Augustine (dfunc, abund, automated) seem to now handle NAs in
    distances, but not group size etc.

--- Rdistance/R/vignette.example.R
+++ Rdistance/R/vignette.example.R
@@ -7,7 +7,16 @@
 # and intern at WEST, Inc.
 # jason.d.carlisle@gmail.com
 
-# Last updated 1/28/2015
+# Assistance from Trent McDonald, WEST, Inc.
+
+# Last updated 2/2/2015
+
+
+
+# Source current functions from CarlisleWorkspace branch
+fundir <- "C:/Users/jcarlis3/Box Sync/Classes/s8_Spr_15/DistanceSampling/Rdistance/Rdistance/R"
+source(paste(fundir, "perp.dists.R", sep="/"))
+source(paste(fundir, "F.dfunc.estim.R", sep="/"))
 
 
 # Load or Install/load the Rdistance package (currently version 1.1)
@@ -16,41 +25,85 @@ if(!require(Rdistance)) {
     require(Rdistance)
 }
 
-# Set working directory
-setwd("C:\\Users\\jcarlis3\\Box Sync\\Classes\\s8_Spr_15\\DistanceSampling\\ExampleVignette")
 
+# Load example dataset
+load("C:/Users/jcarlis3/Box Sync/Classes/s8_Spr_15/DistanceSampling/Rdistance/Rdistance/data/sparrows.rda")
 
-# Read in example datasets
-counts <- read.csv("Sparrows.Counts.csv")
-covs <- read.csv("Sparrows.Covariates.csv")
-
+# data(sparrows)
 
 
 # Add NA rows to counts where no sparrows were observed
-unique(covs$TranID)
-absences <- data.frame(table(counts$TranID))
+length(unique(sparrows.covs$TranID))  # unique IDs for all transects surveyed (even if no sparrow recorded)
+absences <- data.frame(table(sparrows.counts$TranID))  # Note sparrows not observed at all transects
 absences <- as.character(absences[absences$Freq==0, 1])
 
-(toadd <- data.frame(matrix(nrow=length(absences), ncol=ncol(counts))))
+(toadd <- data.frame(matrix(nrow=length(absences), ncol=ncol(sparrows.counts))))
 toadd[, 1] <- absences
-names(toadd) <- names(counts)
+names(toadd) <- names(sparrows.counts)
 
-counts <- rbind(counts, toadd)
+# Append NA rows
+sparrows.counts <- rbind(sparrows.counts, toadd)
 
 
 
 
 
 # Compute the off-transect (aka perpendicular) distances from the observer's sight distance and angle
-counts$PerpDist <- perp.dists(obs.dist=counts$SightDist, obs.angle=counts$SightAngle, digits=1)
+sparrows.counts$PerpDist <- perp.dists(obs.dist=sparrows.counts$SightDist, obs.angle=sparrows.counts$SightAngle, digits=1)
 
-# Save vector of distances
-x <- counts$PerpDist
+# Save vector of distances (includes NAs)
+x <- sparrows.counts$PerpDist
 
 # Examine the histogram of distances
 hist(x)
 summary(x)
 
+
+
+
+# Fit detection function
+?F.dfunc.estim
+(dfunc <- F.dfunc.estim(x, likelihood="uniform", w.hi=150))
+# (dfunc <- F.dfunc.estim(x, likelihood="halfnorm", w.hi=150))
+# (dfunc <- F.dfunc.estim(x, likelihood="hazrate", w.hi=150))
+
+
+# 72 transects surveyed, each 500 m
+# area=10000 converts to density per ha (for dist measured in m)
+?F.abund.estim
+#(fit <- F.abund.estim(dfunc, group.sizes=sparrows.counts$Number, tot.trans.len=(72*500), area=10000, plot.bs=TRUE))
+(fit <- F.abund.estim(dfunc, tot.trans.len=(72*500), area=10000, R=200, plot.bs=TRUE))
+print(fit)
+str(fit)
+
+# How to index abundance estimate?
+fit
+# What are the names?
+fit$ci
+
+# plot
+fit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Next sections to tackle
+
+
+
+?F.abund.estim
+?F.gx.estim
 
 # Throws error prior to bootstrapping:
 #Error in key * (1 + (exp.term %*% a[2:(nexp + 1)])) : 
@@ -72,13 +125,18 @@ F.automated.CDA(x, area=10000, total.trans.len=(72*500), w.hi=150,
 
 
 
-# Fit detection function
-# from ?F.dfunc.estim
-
-(fit <- F.dfunc.estim(x, likelihood="uniform", w.hi=150))
-
-(fit <- F.abund.estim(fit, tot.trans.len=(72*500), area=10000))
-print(fit)
 
 
 
+
+
+
+
+## Junk code
+# # Set working directory
+# setwd("C:\\Users\\jcarlis3\\Box Sync\\Classes\\s8_Spr_15\\DistanceSampling\\ExampleVignette")
+
+
+# # Read in example datasets
+# counts <- read.csv("Sparrows.Counts.csv")
+# covs <- read.csv("Sparrows.Covariates.csv")
