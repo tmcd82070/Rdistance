9a68d5e361a0dbf17d972fa6feceee38f1978fc2
Jason Carlisle <jason.d.carlisle@gmail.com>
2015-02-26 08:24:07 -0700

    Updated version again ...
    It didn't like 1.1.1 (too many decimals), so I am going with 1.2
    instead.

--- Rdistance/R/zzz.R
+++ Rdistance/R/zzz.R
@@ -2,7 +2,7 @@
 
     #v <- packageVersion("Rdistance")  # this requires utils package, and I don't want to make this dependent on Utils
     
-    v <- 1.1.1
+    v <- 1.2
 
     packageStartupMessage( paste("Rdistance - Distance and Line Transect Analysis (vers ", v ,")", sep=""))  # You have to change this every version
 	packageStartupMessage("\nWEST Inc. (tmcdonald@west-inc.com)") 
