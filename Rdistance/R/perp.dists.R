25fec8ae94259a57334d01f3a84f6b20338aaaab
Jason Carlisle <jason.d.carlisle@gmail.com>
2015-03-10 13:02:15 -0600

    New help doc for perp.dists.R and minor change to function
    Added a default rounding digits value to the function, and created a
    help documentation file.

--- Rdistance/R/perp.dists.R
+++ Rdistance/R/perp.dists.R
@@ -1,4 +1,4 @@
-perp.dists <- function(obs.dist, obs.angle, digits){
+perp.dists <- function(obs.dist, obs.angle, digits=1){
     
     # For line transect surveys where the distance from the observer (obs.dist) and angle (in degrees) from
     # the line (obs.angle) is recorded.
