86048d71b546cb488455e84903ab8c582e5f64b1
tmcd <tmcdonald@west-inc.com>
2015-03-13 14:45:49 -0600

    Changes to documentation
    Changes in documentation, mostly in the examples

--- Rdistance/R/AIC.dfunc.R
+++ Rdistance/R/AIC.dfunc.R
@@ -1,5 +1,5 @@
 AIC.dfunc=function (object, ..., k = 2, n = length(object$dist)) 
 {
   p <- length(coef(object))
-  k*p + 2*object$loglik + (2 * p * (p + 1))/(n - p - 1)
+  k*p + 2*object$loglik + (k * p * (p + 1))/(n - p - 1)
 }
