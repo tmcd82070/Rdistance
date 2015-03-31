402a0cc0c0e9e5ce1a1e981b7ca6144ce55a4bcb
Jason Carlisle <jason.d.carlisle@gmail.com>
2015-03-31 01:12:59 -0600

    Update/rename sparrow example vignette

--- Rdistance/vignettes/vignette.sparrow.example.R
+++ Rdistance/vignettes/vignette.sparrow.example.R
@@ -1,5 +1,5 @@
 # Vignette for the Rdistance Package
-# Distance-sampling analysis in Rdistance
+# Distance-sampling analysis in Rdistance using the sparrow dataset
 
 # Prepared by:
 # Jason D. Carlisle
@@ -8,7 +8,7 @@
 # jason.d.carlisle@gmail.com
 # Assistance from Trent L. McDonald, WEST, Inc.
 
-# Last updated 3/25/2015
+# Last updated 3/30/2015
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
 # Install package ---------------------------------------------------------
@@ -17,31 +17,25 @@
 # Install devtools in order to install development version of Rdistance from GitHub
 require(devtools)
 
-# Some functionality of devtools depends on Rtools.  install_github() apparently does not.
-# If needed though, download Rtools from web at http://cran.r-project.org/bin/windows/Rtools/
-# For R version 3.1.2, use Rtools31.exe
-
 # Install the development version of Rdistance from the CarlisleWorkspace branch on GitHub
 install_github("tmcd82070/Rdistance/Rdistance@CarlisleWorkspace")
-
 require(Rdistance)
-
 
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
 # Import and prep data ----------------------------------------------------
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
-# Load the example sparrows dataset from package
-data(sparrows)
+# Load the example sparrows distances dataset from package
+data(sparrow.dists)
 
-# Compute the off-transect (aka perpendicular) distances from the observer's sight distance and angle
-dists.df$dists <- perp.dists(obs.dist=dists.df$sightdist, obs.angle=dists.df$sightangle, digits=1)
+# Compute perpendicular, off-transect distances from the observer's sight distance and angle
+sparrow.dists$dists <- perp.dists(obs.dist=sparrow.dists$sightdist, obs.angle=sparrow.dists$sightangle)
 
 # Remove sight distance and angle
-dists.df <- dists.df[, -which(names(dists.df) %in% c("sightdist", "sightangle"))]
+sparrow.dists <- sparrow.dists[, -which(names(sparrow.dists) %in% c("sightdist", "sightangle"))]
 
 # Save vector of distances
-x <- dists.df$dists
+x <- sparrow.dists$dists
 
 # Examine the histogram of distances
 hist(x)
@@ -56,11 +50,10 @@ summary(x)
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
 # Fit detection function
 # Requires only a vector of distances
-?F.dfunc.estim  # outdated -- need to update that dist argument can take two forms (see below)
 (dfunc <- F.dfunc.estim(x, likelihood="halfnorm", w.hi=150))  # supplying just the vector of distances
 
 # same as supplying the data.frame with "dists" column
-F.dfunc.estim(dists.df, likelihood="halfnorm", w.hi=150)
+F.dfunc.estim(sparrow.dists, likelihood="halfnorm", w.hi=150)
 
 
 
@@ -83,15 +76,13 @@ ESW(dfunc)  # found it, didn't realize it wasn't stored in dfunc directly
 # Requires two data.frames:
   # a distance data.frame with at least $distance, $siteID, and $groupsize
   # a site-covariate data.frame with at least $siteID and $length
+data(sparrow.covs)
 
 
 # 72 transects surveyed, each 500 m
 # area=10000 converts to density per ha (for dist measured in m)
 
-
-?F.abund.estim  # really outdated -- group sizes and transect lengths etc. handled differently (see inside function)
-
-(fit <- F.abund.estim(dfunc, distdata=dists.df, covdata=covs.df, area=10000,
+(fit <- F.abund.estim(dfunc, distdata=sparrow.dists, covdata=sparrow.covs, area=10000,
                       R=500, ci=0.95, plot.bs=TRUE, bs.method="transects"))
 
 
@@ -126,7 +117,7 @@ plot(fit)
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
 
 
-F.automated.CDA(distdata=dists.df, covdata=covs.df, area=10000, R=500, ci=0.95, plot.bs=TRUE,
+F.automated.CDA(distdata=sparrow.dists, covdata=sparrow.covs, area=10000, R=500, ci=0.95, plot.bs=TRUE,
                 bs.method="transects", w.hi=150)
 
 
@@ -137,8 +128,8 @@ F.automated.CDA(distdata=dists.df, covdata=covs.df, area=10000, R=500, ci=0.95, 
 
 
 # Compare to fitting the best model outside of automation
-(dfunc <- F.dfunc.estim(dists.df, likelihood="negexp", expansions=1, series="cosine", w.hi=150))
-F.abund.estim(dfunc, distdata=dists.df, covdata=covs.df, area=10000, R=500, ci=0.95, plot.bs=TRUE, bs.method="transects")
+(dfunc <- F.dfunc.estim(sparrow.dists, likelihood="negexp", expansions=1, series="cosine", w.hi=150))
+F.abund.estim(dfunc, distdata=sparrow.dists, covdata=sparrow.covs, area=10000, R=500, ci=0.95, plot.bs=TRUE, bs.method="transects")
 
 
 
@@ -146,21 +137,12 @@ F.abund.estim(dfunc, distdata=dists.df, covdata=covs.df, area=10000, R=500, ci=0
 # Example with two groups -------------------------------------------------
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
 
-data(sparrows)
-
-# Compute the off-transect (aka perpendicular) distances from the observer's sight distance and angle
-dists.df$dists <- perp.dists(obs.dist=dists.df$sightdist, obs.angle=dists.df$sightangle, digits=1)
-
-# Remove sight distance and angle
-dists.df <- dists.df[, -which(names(dists.df) %in% c("sightdist", "sightangle"))]
-
-
 # split the covariats and distance dataframe by sagebrush cover class
-hi.covs <- covs.df[covs.df$sage=="High", ]
-lo.covs <- covs.df[covs.df$sage=="Low", ]
+hi.covs <- sparrow.covs[sparrow.covs$sage=="High", ]
+lo.covs <- sparrow.covs[sparrow.covs$sage=="Low", ]
 
-hi.dists <- dists.df[dists.df$siteID %in% hi.covs$siteID, ]
-lo.dists <- dists.df[dists.df$siteID %in% lo.covs$siteID, ]
+hi.dists <- sparrow.dists[sparrow.dists$siteID %in% hi.covs$siteID, ]
+lo.dists <- sparrow.dists[sparrow.dists$siteID %in% lo.covs$siteID, ]
 
 # histograms
 hist(hi.dists$dists)
@@ -203,10 +185,10 @@ ggplot(data=pdata, aes(x=sage, y=nhat, fill=sage)) +
 ##
 # automated option
 (hi.fit <- F.automated.CDA(distdata=hi.dists, covdata=hi.covs, area=10000, R=200, ci=0.95,
-                           plot.bs=TRUE, bs.method="transects", w.hi=150))
+                           plot.bs=TRUE, bs.method="transects", w.hi=150, plot=FALSE))
 
 (lo.fit <- F.automated.CDA(distdata=lo.dists, covdata=lo.covs, area=10000, R=200, ci=0.95,
-                           plot.bs=TRUE, bs.method="transects", w.hi=150))
+                           plot.bs=TRUE, bs.method="transects", w.hi=150, plot=FALSE))
 
 
 
@@ -236,33 +218,6 @@ ggplot(data=pdata, aes(x=sage, y=nhat, fill=sage)) +
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
 # Junk code ---------------------------------------------------------------
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
-
-
-## Next sections to tackle
-
-
-
-# ?F.abund.estim
-# ?F.gx.estim
-# 
-# # Throws error prior to bootstrapping:
-# #Error in key * (1 + (exp.term %*% a[2:(nexp + 1)])) : 
-# #    non-conformable arrays
-# F.automated.CDA(x, area=10000, total.trans.len=500, w.hi = 150,
-#                 likelihoods=c("uniform", "halfnorm", "hazrate", "negexp", "Gamma"),
-#                 series=c("simple", "cosine"), expansions=c(0, 1, 2, 3), plot=TRUE)
-# 
-# # uniform, 0 expansions selected as best
-# # density (per ha) is 0.96 (CI=0.84, 1.19)
-# F.automated.CDA(x, area=10000, total.trans.len=(72*500), w.hi = 150,
-#                 likelihoods=c("uniform", "halfnorm", "Gamma"),
-#                 series=c("simple", "cosine"), expansions=c(0, 1, 2), plot=TRUE)
-# 
-# F.automated.CDA(x, area=10000, total.trans.len=(72*500), w.hi=150,
-#                 likelihoods=c("halfnorm", "uniform", "hazrate", "negexp", "Gamma"),
-#                 series=c("simple", "cosine"), plot=TRUE)
-# 
-
 
 
 
