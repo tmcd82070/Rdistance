74bb495fdb553affe22775ce5040ec4e25c06146
Jason Carlisle <jason.d.carlisle@gmail.com>
2015-04-01 12:03:51 -0600

    Revert "Update to package message"
    This reverts commit 81529fcd73b300cab071a8f2b86a4d9206f22669.

--- Rdistance/R/zzz.R
+++ Rdistance/R/zzz.R
@@ -1,16 +1,11 @@
-.onAttach <- function(libname, pkgname){
-  
-  # Automatic version number update
-  #v <- packageVersion("Rdistance")  # this requires utils package, and I don't want to make this dependent on Utils  
-  
-  # Manual version number update (you have to change version number yourself)
-  v <- 1.2
-  
-  # Printed message
-  packageStartupMessage(
-    cat("Rdistance (version ", v, ")", "\n",
-          "Distance Sampling Analyses for Line Transect Data", "\n",
-          "WEST Inc. (tmcdonald@west-inc.com)",
-          sep="")
-    )
+.onAttach<-function(libname, pkgname){
+
+    #v <- packageVersion("Rdistance")  # this requires utils package, and I don't want to make this dependent on Utils
+    
+    v <- 1.2
+
+    packageStartupMessage( paste("Rdistance - Distance Sampling Analyses for Line Transect Data (vers ", v ,")", sep=""))  # You have to change this every version
+	packageStartupMessage("\nWEST Inc. (tmcdonald@west-inc.com)") 
+
+
 }
