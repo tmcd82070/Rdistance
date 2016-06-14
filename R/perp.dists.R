b1875b6d6e47fa3f20e9f0e8846db6969e25262e
Jason Carlisle <jason.d.carlisle@gmail.com>
2016-06-13 21:40:49 -0600

    Update to perp.dists function
    Function required specifying two arguments in df$column format.  Changed
    now, so there's a data argument, and the two names are given as
    characters.  Also, removed the rounding -- user can do that on their
    own.

--- R/perp.dists.R
+++ R/perp.dists.R
@@ -1,12 +1,12 @@
-perp.dists <- function(obs.dist, obs.angle, digits=1){
+perp.dists <- function(s.dist, s.angle, data){
   
   # Stop and print error if dist or angle is NA
-  if(any(is.na(obs.dist)))
-    stop("Please remove detections for which obs.dist and/or obs.angle is NA.")
-  if(any(is.na(obs.angle)))
-    stop("Please remove detections for which obs.dist and/or obs.angle is NA.")
+  if(any(is.na(data[, s.dist])))
+    stop("Please remove detections for which s.dist and/or s.angle is NA.")
+  if(any(is.na(data[, s.angle])))
+    stop("Please remove detections for which s.dist and/or s.angle is NA.")
   
   # Convert degrees to radians, then apply sine function and round off decimal places
-  round(obs.dist * sin(obs.angle*(pi/180)), digits)
+  data[, s.dist] * sin(data[, s.angle]*(pi/180))
     
 }
