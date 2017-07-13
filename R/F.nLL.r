0b1a274debb9b9f4304f74620498c11867b2c32d
Aidan McDonald <amcdonald@WEST-INC.LOCAL>
2017-07-12 18:11:24 -0600

    Update to Roxygen documentation

--- R/F.nLL.r
+++ R/F.nLL.r
@@ -1,3 +1,29 @@
+#' @name F.nLL
+#' @aliases F.nLL
+#' @title F.nLL - Return the negative log likelihood for a set of distance values.
+#' @description Return value of the negative log likelihood for a set of observed off-transect distances given a specified likelihood, 
+#'   number of expansion terms, and estimated parameters.
+#' @usage F.nLL(a, dist, covars = NULL, like, w.lo=0, w.hi=max(dist), series, expansions=0, point.transects, for.optim = F)
+#' @param a A vector of parameter values to use to compute the liklihood. Length of this vector must be 
+#'   \code{expansions + 1 + 1*(like \%in\% c("hazrate", "uniform"))}.
+#' @param dist A vector of observed off-transect distances. Must be between \code{w.lo} and \code{w.hi} (see below).
+#' @param covars Data frame containing values of covariates at each observation in \code{dist}.
+#' @param like String specifying the form of the likelihood.  Valid values at present are "uniform", "halfnorm", "hazrate", "negexp", and "Gamma".
+#' @param w.lo Lower or left-truncation limit of the distances.  This is the minimum possible off-transect distance. Default is 0.
+#' @param w.hi Upper or right-truncation limit of the distances. This is the maximum off-transect distance that could be observed. 
+#'   Default is the maximum observed distance.
+#' @param series String specifying the type of expansion to use series if \code{expansions} > 0. Valid values at present are 'simple', 'hermite',
+#'   and 'cosine'.
+#' @param expansions A scalar specifying the number of terms in \code{series} to compute. Depending on the series, this could be 0 through 5. 
+#'   The default of 0 equates to no expansion terms of any type.
+#' @param point.transects Boolean. TRUE if \code{dist} is point transect data, FALSE if line transect data.
+#' @param for.optim Boolean. If TRUE, values are multpilied by 10^9 to help optim converge more consistently.
+#' @value A scalar, the negative of the log likelihood given the parameters in \code{a}, likelihood, and expansion terms.
+#' @author Trent McDonald, WEST, Inc. \email{tmcdonald@west-inc.com}
+#'         Aidan McDonald, WEST, Inc. \email{aidan@mcdcentral.org}
+#' @seealso See \code{\link{uniform.like}} for definitions of all the likelihoods; \code{\link{F.dfunc.estim}}
+#' @keywords models
+
 F.nLL <- function(a, dist, covars = NULL, like, w.lo=0, w.hi=max(dist), series, expansions=0, point.transects, for.optim = F){
 
     f.like <- match.fun(paste( like, ".like", sep=""))
--- R/perp.dists.R
+++ R/perp.dists.R
@@ -1,3 +1,26 @@
+#' @name perp.dists
+#' @alias perp.dists
+#' @title Compute off-transect ditances from sighting distances and angles
+#' @description Computes off-transect (also called 'perpendicular') distances from measures of sighting distance and sighting angle.
+#' @usage perp.dists(s.dist, s.angle, data)
+#' @param s.dist Character, name of column in \code{data} that contains the observed or sighting distances from the observer to the detected objects.
+#' @param s.angle Character, name of column in \code{data} that contains the observed or sighting angles from the line transect to the detected 
+#'   objects.  Angles must be measured in degrees.
+#' @param data data.frame object containing sighting distance and sighting angle.
+#' @details If observers recorded sighting distance and sighting angle (as is often common in line transect surveys), use this function to convert 
+#'   to off-transect distances, the required input data for \code{F.dfunc.estim}.
+#' @value A vector of off-transect (or perpendicular) distances.  Units are the same as \code{obs.dist}.
+#' @references Buckland, S.T., Anderson, D.R., Burnham, K.P. and Laake, J.L. 1993. 
+#'   \emph{Distance Sampling: Estimating Abundance of Biological Populations}. Chapman and Hall, London.
+#' @author Jason Carlisle, University of Wyoming, \email{jason.d.carlisle@gmail.com}
+#' @seealso \code{\link{F.dfunc.estim}}
+#' @examples # Load the example dataset of sparrow detections from package
+#' data(sparrow.detections)
+#' # Compute perpendicular, off-transect distances from the observer's sight distance and angle
+#' sparrow.detections$dist <- perp.dists(s.dist="sightdist", s.angle="sightangle",
+#'                                       data=sparrow.detections)
+#' # Proceed to fitting the detection function with F.dfunc.estim
+
 perp.dists <- function(s.dist, s.angle, data){
   
   # Stop and print error if dist or angle is NA
