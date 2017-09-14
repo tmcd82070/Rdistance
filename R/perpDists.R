d3ed658fdb128332d6d812846d6f037650ed4e00
jcarlis3 <jcarlisle@west-inc.com>
2017-09-14 17:03:49 -0600

    Renamed perpDists function and arguments
    Renamed to conform to camelCase style.

--- R/perpDists.R
+++ R/perpDists.R
@@ -1,15 +1,15 @@
-#' @name perp.dists
-#' @aliases perp.dists
+#' @name perpDists
+#' @aliases perpDists
 #' @title Compute off-transect ditances from sighting distances and angles
 #' @description Computes off-transect (also called 'perpendicular') distances from measures of sighting distance and sighting angle.
-#' @usage perp.dists(s.dist, s.angle, data)
-#' @param s.dist Character, name of column in \code{data} that contains the observed or sighting distances from the observer to the detected objects.
-#' @param s.angle Character, name of column in \code{data} that contains the observed or sighting angles from the line transect to the detected 
+#' @usage perpDists(sightDist, sightAngle, data)
+#' @param sightDist Character, name of column in \code{data} that contains the observed or sighting distances from the observer to the detected objects.
+#' @param sightAngle Character, name of column in \code{data} that contains the observed or sighting angles from the line transect to the detected 
 #'   objects.  Angles must be measured in degrees.
 #' @param data data.frame object containing sighting distance and sighting angle.
 #' @details If observers recorded sighting distance and sighting angle (as is often common in line transect surveys), use this function to convert 
 #'   to off-transect distances, the required input data for \code{F.dfunc.estim}.
-#' @return A vector of off-transect (or perpendicular) distances.  Units are the same as \code{obs.dist}.
+#' @return A vector of off-transect (or perpendicular) distances.  Units are the same as \code{sightDist}.
 #' @references Buckland, S.T., Anderson, D.R., Burnham, K.P. and Laake, J.L. 1993. 
 #'   \emph{Distance Sampling: Estimating Abundance of Biological Populations}. Chapman and Hall, London.
 #' @author Jason Carlisle, University of Wyoming, \email{jason.d.carlisle@gmail.com}
@@ -17,20 +17,20 @@
 #' @examples # Load the example dataset of sparrow detections from package
 #' data(sparrow.detections)
 #' # Compute perpendicular, off-transect distances from the observer's sight distance and angle
-#' sparrow.detections$dist <- perp.dists(s.dist="sightdist", s.angle="sightangle",
+#' sparrow.detections$dist <- perpDists(sightDist="sightdist", sightAngle="sightangle",
 #'                                       data=sparrow.detections)
 #' # Proceed to fitting the detection function with F.dfunc.estim
 #' @export
 
-perp.dists <- function(s.dist, s.angle, data){
+perpDists <- function(sightDist, sightAngle, data){
   
   # Stop and print error if dist or angle is NA
-  if(any(is.na(data[, s.dist])))
-    stop("Please remove detections for which s.dist and/or s.angle is NA.")
-  if(any(is.na(data[, s.angle])))
-    stop("Please remove detections for which s.dist and/or s.angle is NA.")
+  if(any(is.na(data[, sightDist])))
+    stop("Please remove detections for which sightDist and/or sightAngle is NA.")
+  if(any(is.na(data[, sightAngle])))
+    stop("Please remove detections for which sightDist and/or sightAngle is NA.")
   
   # Convert degrees to radians, then apply sine function and round off decimal places
-  data[, s.dist] * sin(data[, s.angle]*(pi/180))
+  data[, sightDist] * sin(data[, sightAngle]*(pi/180))
     
 }
