4b43e4cf743c40b01dd4bf551760cb420d6fd3d5
jcarlisle <jcarlisle@west-inc.com>
2017-10-23 15:56:34 -0600

    Updated example in abundEstim
    Example in abundEstim was outdated and throwing an error.  Flagged by CRAN check and fixed here.

--- R/abundEstim.R
+++ R/abundEstim.R
@@ -146,19 +146,25 @@
 #'   
 #' @seealso \code{\link{dfuncEstim}}, \code{\link{autoDistSamp}}.
 #' 
-#' @examples # Load the example datasets of sparrow detections and transects from package
-#'   data(sparrowDetectionData)
-#'   data(sparrowSiteData)
-#'   
-#'   # Fit detection function to perpendicular, off-transect distances
-#'   dfunc <- dfuncEstim(sparrowDetectionData, w.hi=150)
-#'   
-#'   # Estimate abundance given a detection function
-#'   # Note, area=10000 converts to density per hectare (for distances measured in meters)
-#'   # Note, a person should do more than R=20 iterations 
-#'   fit <- abundEstim(dfunc, detectionData=sparrowDetectionData, 
-#'          siteData=sparrowSiteData, area=10000, R=20, ci=0.95, 
-#'          plot.bs=TRUE, by.id=FALSE)
+#' @examples
+#' # Load example sparrow data (line transect survey type)
+#' data(sparrowDetectionData)
+#' data(sparrowSiteData)
+#' 
+#' # Fit half-normal detection function
+#' dfunc <- dfuncEstim(formula=dist~1,
+#'                     detectionData=sparrowDetectionData,
+#'                     likelihood="halfnorm", w.hi=100, pointSurvey=FALSE)
+#' 
+#' # Estimate abundance given a detection function
+#' # Note, area=10000 converts to density per hectare (for distances measured in meters)
+#' # Note, a person should do more than R=20 iterations
+#' fit <- abundEstim(dfunc, detectionData=sparrowDetectionData,
+#'                   siteData=sparrowSiteData, area=10000, R=20, ci=0.95,
+#'                   plot.bs=TRUE, bySite=FALSE)
+#' 
+#' # Print results
+#' fit
 #'          
 #' @keywords model
 #' @export
