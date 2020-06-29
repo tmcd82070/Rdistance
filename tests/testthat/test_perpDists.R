f9c8c11a261e5de0172db708f00218d416a9370e
wmcdonald1 <wmcdonald@west-inc.com>
2020-06-29 14:40:49 -0600

    Update test_perpDists.R
    added error catch for NA's in distance and angle data

--- tests/testthat/test_perpDists.R
+++ tests/testthat/test_perpDists.R
@@ -6,15 +6,15 @@ library(Rdistance)
 
 testData <- data.frame(distance.radial = c(3, 9, 27), angle.degrees = c(30, 45, 60))
 
-## Create a test data.frame with an unintended NA in distance.radial for error
+## Create a test data.frame with an unintended NA in distance.radial and angle.degrees for error
 ## catch testing
-testDataNA <- data.frame(distance.radial = c(3, NA, 27), angle.degrees = c(30, 45, 60))
+testDataNA <- data.frame(distance.radial = c(3, NA, 27), angle.degrees = c(NA, 45, 60))
 ## Context
 context("testing perpDists()")
 
 test_that("perpDists() operates appropriatly", {
   
-  ## Make sure error catch works for NA in distance
+  ## Make sure error catch works for NAs in testDataNA
   expect_error(perpDists(sightDist = "distance.radial", sightAngle = "angle.degrees",  
   testDataNA), "Please remove detections for which sightDist and/or sightAngle is NA.")
   
