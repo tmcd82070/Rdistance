417e9cd2ef8c75bba1033ca893a2968449c96cea
Reid Olson <rolson@WEST-INC.LOCAL>
2020-07-10 09:55:16 -0600

    changed names of .rds files; beginning of files now have function name: e.g. edr_dfunc_point_covar.rds

--- tests/testthat/test_EDR.r
+++ tests/testthat/test_EDR.r
@@ -34,7 +34,7 @@ dfunc_point_case_1 <- dfuncEstim(formula = dist ~ 1,
 # file is located in testthat folder; delete file to reset stored value
 # dfunc_point_case_1 enters 1st if statement in EDR
 test_that("EDR(dfunc_point_case_1) returns equivalent obj as it did previously", {
-  expect_equal_to_reference(EDR(dfunc_point_case_1), "dfunc_point_case_1.rds")
+  expect_equal_to_reference(EDR(dfunc_point_case_1), "edr_dfunc_point_case_1.rds")
 })
 
 data("thrasherDetectionData")
@@ -50,9 +50,9 @@ newdata <- data.frame(observer=factor(c("obs1", "obs2"), levels = paste0("obs", 
 
 # skip missing data case
 test_that("EDR(dfunc_point_covar, newdata) returns equivalent obj as it did previously", {
-  expect_equal_to_reference(EDR(dfunc_point_covar, newdata), "dfunc_point_covar_newdata.rds")
+  expect_equal_to_reference(EDR(dfunc_point_covar, newdata), "edr_dfunc_point_covar_newdata.rds")
 })
 # enter missing data case
 test_that("EDR(dfunc_point_covar) returns equivalent obj as it did previously", {
-  expect_equal_to_reference(EDR(dfunc_point_covar), "dfunc_point_covar.rds")
+  expect_equal_to_reference(EDR(dfunc_point_covar), "edr_dfunc_point_covar.rds")
 })
