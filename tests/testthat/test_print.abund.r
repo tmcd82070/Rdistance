3fe70af493a5484ab62e77354c85f720575b178e
Reid Olson <rolson@WEST-INC.LOCAL>
2020-08-10 12:43:15 -0600

    added capture.output to suppress console ouput

--- tests/testthat/test_print.abund.r
+++ tests/testthat/test_print.abund.r
@@ -7,9 +7,9 @@ data(sparrowSiteData)
 dfunc <- dfuncEstim(formula=dist~1,
                     detectionData=sparrowDetectionData,
                     likelihood="halfnorm", w.hi=100, pointSurvey=FALSE)
-fit <- abundEstim(dfunc, detectionData=sparrowDetectionData,
+invisible(capture.output(fit <- abundEstim(dfunc, detectionData=sparrowDetectionData,
                   siteData=sparrowSiteData, area=10000, R=20, ci=0.95,
-                  plot.bs=TRUE, bySite=FALSE)
+                  plot.bs=TRUE, bySite=FALSE)))
 
 fit$nItersConverged <- 2
 test_that("print.abund(fit: nItersConverged < length(B)) returns expected warning", {
