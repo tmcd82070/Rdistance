849c0a433c95f10d30ea97d93001223422b7a6b7
Reid Olson <rolson@WEST-INC.LOCAL>
2020-08-10 11:27:32 -0600

    removing expectation from test; abundEstim function seems to generate different confidence intervals even when run on same dataset

--- tests/testthat/test_print.abund.r
+++ tests/testthat/test_print.abund.r
@@ -11,10 +11,6 @@ fit <- abundEstim(dfunc, detectionData=sparrowDetectionData,
                   siteData=sparrowSiteData, area=10000, R=20, ci=0.95,
                   plot.bs=TRUE, bySite=FALSE)
 
-test_that("print.abund(fit) returns equivalent ojbect as it did previously", {
-  expect_equal_to_reference(capture.output(print.abund(fit)), "print.abund.rds")
-})
-
 fit$nItersConverged <- 2
 test_that("print.abund(fit: nItersConverged < length(B)) returns expected warning", {
   expect_warning(print.abund(fit), "The proportion of non-convergent bootstrap iterations is high.")
