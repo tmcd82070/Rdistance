0c51c56fc857dec19cb5704e2e0fa3f74ead096f
Trent McDonald <trent@mcdonalddatasciences.com>
2023-12-27 11:00:01 -0700

    Change to controls from RdistanceControls to callst to options()
    Before: controls were specified in call to Rdistance::RdistanceControls().
    
    After: controls specified in call to options(). RdistanceControls.R is not just documentation, no code. zzz.R replaced with .onAttach.R.  Call to options() with default values added to .onAttach().
    
    files committed:
      R/RdistanceControls.R
      R/zzz.R - Deleted
      R/.onAttach.R

--- R/zzz.R
+++
@@ -1,9 +0,0 @@
-.onAttach <- function(libname, pkgname){
-  
-  # To automatically update the version number  
-  v <- utils::packageVersion("Rdistance")  # this requires utils package, which is base
-  
-  # Startup message
-  packageStartupMessage(paste("Rdistance (version ", v ,")", sep=""))
-
-}
