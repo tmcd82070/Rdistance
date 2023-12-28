75ff34919f76ac8c2e6cbebcb5f2f3866d6e2502
Trent McDonald <trent@mcdonalddatasciences.com>
2023-12-28 15:07:04 -0700

    Changes to Rdistance controls documentation
    Before: used invalid \describe list
    
    After: used \itemize list structure. added required @name
    
    files committed:
      r/RdistanceControls.R

--- R/RdistanceControls.R
+++ R/RdistanceControls.R
@@ -1,32 +1,34 @@
-#' @title Rdistance options - Parameters for \code{Rdistance} optimization.
+#' @title Rdistance optimization control parameters.
 #' 
-#' @aliases control, controls, RdistanceControls
+#' @aliases control controls RdistanceControls
 #' 
-#' @concept control, optimization
+#' @concept control optimization
 #' 
-#' @description Control parameters used in 
-#' \code{Rdistance} set by calls to \code{options()}.
+#' @description Optimization control parameters 
+#' are set by calls to \code{options()} (see examples). 
+#' Optimization parameters used in 
+#' \code{Rdistance} are the following:  
 #' 
-#' \decribe{
-#'   \item{Rdist_maxIters}{The maximum number of optimization 
-#' iterations allowed.}
+#' \itemize{
+#'   \item \code{Rdist_maxIters}: The maximum number of optimization 
+#' iterations allowed.
 #' 
-#'   \item{Rdist_evalMax}{The maximum number of objective function
-#' evaluations allowed.}
+#'   \item \code{Rdist_evalMax}: The maximum number of objective function
+#' evaluations allowed.
 #' 
-#'   \item{Rdist_likeTol}{Minimum change in the likelihood 
+#'   \item \code{Rdist_likeTol}: Minimum change in the likelihood 
 #' between iterations required optimization to continue.  
 #' If the likelihood changes by less than this amount, 
 #' optimization stops and a solution is declared. Iteration 
-#' continues when likelihood changes exceed this value.} 
+#' continues when likelihood changes exceed this value.
 #' 
-#'   \item{Rdist_coefTol}{Minimum change in model coefficients 
+#'   \item \code{Rdist_coefTol}: Minimum change in model coefficients 
 #' between iterations for optimization to continue.  
 #' If the sum of squared coefficient differences changes 
 #' by less than this amount between iterations, 
-#' optimization stops and a solution is declared. }
+#' optimization stops and a solution is declared. 
 #'
-#'   \item{Rdist_optimizer}{A string specifying the optimizer 
+#'   \item \code{Rdist_optimizer}: A string specifying the optimizer 
 #' to use.  Results can vary between optimizers, so 
 #' switching algorithms sometimes makes a poorly 
 #' behaved distance function converge.  Valid 
@@ -34,17 +36,17 @@
 #' and "nlminb" which uses \code{stats:nlminb}.  The authors 
 #' have had better luck with "nlminb" than "optim" and "nlminb" 
 #' runs noticeably faster.  Problems with solutions near, but not on,
-#' parameter boundaries may require use of "optim".}   
+#' parameter boundaries may require use of "optim".   
 #'
-#'   \item{Rdist_hessEps}{A vector of parameter distances used during 
+#'   \item \code{Rdist_hessEps}: A vector of parameter distances used during 
 #' computation of numeric second derivatives. These distances control
 #' and determine variance estimates, and they may need revision when 
 #' the maximum likelihood solution is near a parameter boundary. 
 #' Should have length 
 #' 1 or the number of parameters in the model. See function 
-#' \code{\link{secondDeriv}} for further details.} 
+#' \code{\link{secondDeriv}} for further details. 
 #'  
-#'   \item{Rdist_requireUnits}{A logical specifying whether measurement 
+#'   \item \code{Rdist_requireUnits}: A logical specifying whether measurement 
 #' units are required on distances and areas.  If TRUE, 
 #' measurement units are required on off-transect and radial 
 #' distances in the input data frame.  Likewise, measurement 
@@ -62,13 +64,13 @@
 #' An error is thrown if differing units are not compatible.  
 #' For example, "m" (meters) cannot be converted into "ha" (hectares),
 #' but "acres" can be converted into "ha".
-#' \code{Rdistance} recognizes units listed in \code{units::valid_udunits()}.} 
+#' \code{Rdistance} recognizes units listed in \code{units::valid_udunits()}. 
 #' 
-#'   \item{Rdist_maxBSFailPropForWarning}{The proportion of bootstrap 
+#'   \item \code{Rdist_maxBSFailPropForWarning}: The proportion of bootstrap 
 #' iterations that can fail without a warning. If the proportion 
 #' of non-convergent bootstrap iterations exceeds this 
 #' parameter, a warning about the validity of CI's is issued in 
-#' the abundance print method. }
+#' the abundance print method. 
 #' 
 #' 
 #' }
@@ -80,4 +82,5 @@
 #' # change optimizer and decrease tolerance
 #' options(list(Rdist_optimizer="optim", Rdist_likeTol=1e-6)) 
 #' 
+#' @name RdistanceControls
 NULL
