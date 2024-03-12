056b3e27de9e5ccd1158db38649716fcd2939e06
Trent <trent@mcdonalddatasceinces.com>
2024-03-12 17:04:19 -0600

    Initial version of test_parseModel.R

--- tests/testthat/test_parseModel.R
+++ tests/testthat/test_parseModel.R
@@ -4,26 +4,69 @@ library(Rdistance)
 sparrowDf <- Rdistance::RdistDf(sparrowSiteData, sparrowDetectionData)
 is.RdistDf(sparrowDf)
 
-parseModel(sparrowDf, dist ~ 1)
 
 # no units on dist
+tmpDf <- sparrowDetectionData |> 
+  dplyr::mutate(dist = units::drop_units(dist))
+tmpRDf <- Rdistance::RdistDf(sparrowSiteData, tmpDf)
+
+testthat::test_that("Error-No Units on dists",{
+  testthat::expect_error(parseModel(tmpRDf, dist ~ 1), "Measurement units required on distances")
+})
 
 # no units on w.lo
+testthat::test_that("Error-No Units on w.lo > 0",{
+  testthat::expect_error(parseModel(sparrowDf
+                                    , dist ~ 1
+                                    , w.lo = 1), "Measurement units required on minimum")
+})
 
 # w.lo equal 0 with no units
+testthat::test_that("w.lo = 0 with no Units",{
+  testthat::expect_type(parseModel(sparrowDf
+                                    , dist ~ 1
+                                    , w.lo = 0), "list")
+})
 
 # no units on w.hi
+testthat::test_that("Error-No Units on w.hi",{
+  testthat::expect_error(parseModel(sparrowDf
+                                    , dist ~ 1
+                                    , w.hi = 300), "Measurement units required on maximum")
+})
 
 # w.hi equal NULL
+testthat::test_that("w.hi = NULL",{
+  testthat::expect_type(parseModel(sparrowDf
+                                   , dist ~ 1
+                                   , w.hi = NULL), "list")
+})
 
-testthat::test_that("Missing formula errors", {
+# w.hi has units
+testthat::test_that("w.hi = 2000 ft",{
+  testthat::expect_type(parseModel(sparrowDf
+                                   , dist ~ 1
+                                   , w.hi = units::set_units(2000, "ft")), "list")
+})
+
+# Missing formula
+testthat::test_that("Error-Missing formula", {
   testthat::expect_error(parseModel(sparrowDf), "'formula' is required")
 })
 
-test_that("AIC(dfunc, criterion=BIC) returns equivalent obj as it did previously", {
-  expect_equal_to_reference(AIC(dfunc, criterion="BIC"), "AIC_dfunc_BIC.rds")
+# Units on dist assigned correctly
+testthat::test_that("Units on dist assigned to outputUnits", {
+  testthat::expect_equal(parseModel(sparrowDf
+                                    , dist ~ 1)$outputUnits, units(sparrowDetectionData$dist))
 })
 
-test_that("AIC(dfunc, criterion=other) returns equivalent obj as it did previously", {
-  expect_equal_to_reference(AIC(dfunc, criterion="other"), "AIC_dfunc_AICc.rds")
+# Output units assigned to dist
+mf <- parseModel(sparrowDf
+               , dist ~ 1
+               , outputUnits = "ft")
+ou <- units::set_units(1, "ft")
+
+testthat::test_that("Units of outputUnits assigned to dist", {
+  testthat::expect_equal(units(stats::model.response(mf$mf)), units(ou))
 })
+
