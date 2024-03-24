0e54ddaad55b37846d634496fe1f899fcc3a2bfa
Trent McDonald <trent@mcdonalddatasciences.com>
2024-03-24 15:18:58 -0600

    Partially complete nll and halfnorm.like
    Computes nLL correctly for no expansion terms. Documentation updated.
    
    files committed:
      R/F.nll.r --> R/nLL.R
      R/halfnorm.like.R

--- R/halfnorm.like.R
+++ R/halfnorm.like.R
@@ -1,88 +1,53 @@
-#' @title Half-normal likelihood function for distance analyses
+#' @title halfnorm.like - Half-normal distance function
 #' 
-#' @description This function computes the likelihood contributions for 
-#' sighting distances, scaled appropriately, for use as a 
-#' distance likelihood.
+#' @description Evaluates the half-normal distance function, 
+#' here called a likelihood, for 
+#' sighting distances based on covariates.
 #' 
-#' @param a A vector of likelihood parameter values. Length and 
-#' meaning depend on \code{series} and \code{expansions}. If no expansion 
-#' terms were called for (i.e., \code{expansions = 0}), the distance 
-#' likelihoods contain one or two canonical parameters (see Details). 
-#' If one or more expansions are called for, coefficients for the 
-#' expansion terms follow coefficients for the canonical parameters.  
-#' i.e., if \code{p} is the number of canonical parameters, coefficients
-#' for the expansion terms are \code{a[(p+1):length(a)]}.
+#' @param a A vector of the likelihood parameter values. Length 
+#' should be (number of covariates, including intercept) + 
+#' (number of expansions). These should all be on a log scale
+#' because link function used is exp. Covariates are related to
+#' standard deviation of the halfnormal distance function. 
 #' 
-#' @param dist A numeric vector containing the observed distances.
+#' @param dist A numeric vector containing 
+#' the observed distances.
 #' 
-#' @param covars Data frame containing values of covariates at 
-#' each observation in \code{dist}.
+#' @param covars A data frame or matrix containing values 
+#' of the covariates for each distance observation. Number of 
+#' columns must match number of parameters (i.e., length of \code{a}).
 #' 
-#' @param w.lo Scalar value of the lowest observable distance.  
-#' This is the \emph{left truncation} of sighting distances 
-#' in \code{dist}. Same units as \code{dist}. Values less than 
-#' \code{w.lo} are allowed in \code{dist}, but are ignored and 
-#' their contribution to the likelihood is set to \code{NA} in the output.
 #' 
-#' @param w.hi Scalar value of the largest observable distance.  
-#' This is the \emph{right truncation} of sighting distances 
-#' in \code{dist}.  Same units as \code{dist}. Values greater 
-#' than \code{w.hi} are allowed in \code{dist}, but are ignored 
-#' and their contribution to the likelihood is set to \code{NA} 
-#' in the output.
+#' @details The half-normal distance function is 
+#' \deqn{f(d|s) = \exp(-d^2 / (2*s^2))}{f(d|s) = exp(-d^2 / (2*s^2))}
+#' where \eqn{s = exp(x'a)}, \eqn{x} is a column vector of 
+#' covariate values associated with distance \eqn{d}, and 
+#' \eqn{a} is a vector of the first \code{ncol(covars)} 
+#' values in the input vector of parameters \code{a}.
 #' 
-#' @param series A string specifying the type of expansion to use.  
-#' Currently, valid values are 'simple', 'hermite', and 'cosine'; but, see 
-#'   \code{\link{dfuncEstim}} about defining other series.
-#'   
-#' @param expansions A scalar specifying the number of terms 
-#' in \code{series}. Depending on the series, this could be 0 through 5.
-#'   The default of 0 equates to no expansion terms of any type.
-#'   
-#' @param scale Logical scalar indicating whether or not to 
-#' scale the likelihood so it integrates to 1. This parameter is 
-#' used to stop recursion in other functions. If \code{scale} 
-#' equals TRUE, a numerical integration routine 
-#' (\code{\link{integration.constant}}) is called, which in turn 
-#' calls this likelihood function again with \code{scale} = FALSE. 
-#' Thus, this routine knows when its values are being used to compute 
-#' the likelihood and when its value is being used to compute the 
-#' constant of integration.  All user defined likelihoods must have 
-#' and use this parameter.
-#' 
-#' @param pointSurvey Boolean. TRUE if distances in \code{dist} are 
-#' radial from point 
-#' transects, FALSE if distances are perpendicular off-transect distances.
-#' 
-#' @details The half-normal likelihood is 
-#' \deqn{f(x|a) = \exp(-x^2 / (2*a^2))}{f(x|a) = exp(-x^2 / (2*a^2))}
-#' where \eqn{a} is the parameter to be estimated.
-#' Some half-normal distance functions in the literature 
+#' Some authors  
 #' do not use a "2" in the 
-#' denominator of the exponent.  \code{Rdistance} uses a 
-#' "2" in the denominator of the exponent to make quantiles of this 
-#' function agree with 
-#' the standard normal which means \emph{a} can be interpreted as a 
-#' normal standard error.  e.g., approximately 95\% of all observations 
-#' will occur between 0 and 2\emph{a}.
+#' denominator of the exponent when defining a half-normal 
+#' distance functions.  \code{Rdistance} uses a 
+#' "2" in the denominator of the exponent to make 
+#' quantiles of this function agree with 
+#' the standard normal. This means \emph{s = exp(x'a)} can be 
+#' interpreted as a normal standard error.  Hence, for example, 
+#' approximately 95\% of distances should 
+#' occur between 0 and 2\emph{s}.
 #' 
-#' \bold{Expansion Terms}: If \code{expansions} = k (k > 0), the expansion function specified by \code{series} is called (see for example
-#'   \code{\link{cosine.expansion}}). Assuming \eqn{h_{ij}(x)}{h_ij(x)} is the \eqn{j^{th}}{j-th} expansion term for the \eqn{i^{th}}{i-th} distance and that 
-#'   \eqn{c_1, c_2, \dots, c_k}{c(1), c(2), ..., c(k)}are (estimated) coefficients for the expansion terms, the likelihood contribution for the \eqn{i^{th}}{i-th} 
-#'   distance is, \deqn{f(x|a,b,c_1,c_2,\dots,c_k) = f(x|a,b)(1 + \sum_{j=1}^{k} c_j h_{ij}(x)).}
-#'   {f(x|a,b,c_1,c_2,...,c_k) = f(x|a,b)(1 + c(1) h_i1(x) + c(2) h_i2(x) + ... + c(k) h_ik(x)). }
 #'   
-#' @return A numeric vector the same length and order as \code{dist} containing the 
-#' likelihood contribution for corresponding distances in \code{dist}. 
-#' Assuming \code{L} is the returned vector from one of these functions, 
-#' the negative log likelihood of all the data is \code{-sum(log(L), na.rm=T)}. 
-#' Note that the returned likelihood value for distances less 
-#' than \code{w.lo} or greater than \code{w.hi} is \code{NA}, 
-#' hence \code{na.rm=TRUE} in the sum. 
-#' If \code{scale} = TRUE, the integral of the likelihood from
-#'  \code{w.lo} to \code{w.hi} is 1.0. If \code{scale} = FALSE, 
-#'  the integral of the likelihood is something else. 
-#' Values are always greater than or equal to zero.
+#' @return A list containing two numeric vectors. One vector, 
+#' named \code{$key}, is the value of the likelihood at every
+#' input distance.  The second vector, named \code{params},
+#' is a vector of likelihood parameters (i.e., \emph{s} in 
+#' equations of Details section) for every input distance. 
+#' Assuming \code{L} is the list returned by this function, 
+#' the negative log likelihood is \code{-sum(log(L$key), na.rm=T)}. 
+#' Note that returned likelihood values for distances less 
+#' than \code{w.lo} or greater than \code{w.hi} are \code{NA}; 
+#' hence, \code{na.rm=TRUE} in the sum. 
+#' Values in the return are always greater than or equal to zero.
 #'  
 #' @seealso \code{\link{dfuncEstim}},
 #'          \code{\link{hazrate.like}},
@@ -93,10 +58,13 @@
 #' @examples  \dontrun{
 #' set.seed(238642)
 #' x <- seq(0, 100, length=100)
+#' ones <- matrix(1, nrow = length(x), ncol = 1)
+#' 
+#' halfnorm.like(log(20), x, ones)
 #' 
 #' # Plots showing effects of changes in parameter Sigma
-#' plot(x, halfnorm.like(20, x), type="l", col="red")
-#' plot(x, halfnorm.like(40, x), type="l", col="blue")
+#' plot(x, halfnorm.like(log(20), x, ones)$key, type="l", col="red")
+#' lines(x, halfnorm.like(log(40), x, ones)$key, col="blue")
 #' 
 #' # Estimate 'halfnorm' distance function
 #' a <- 5
@@ -105,84 +73,26 @@
 #' dfunc <- dfuncEstim(x~1, likelihood="halfnorm")
 #' plot(dfunc)
 #' 
-#' # evaluate the log Likelihood
-#' L <- halfnorm.like(dfunc$parameters, dfunc$detections$dist, covars=dfunc$covars, 
-#'     w.lo=dfunc$w.lo, w.hi=dfunc$w.hi, 
-#'     series=dfunc$series, expansions=dfunc$expansions, 
-#'     scale=TRUE)
-#' -sum(log(L), na.rm=TRUE)  # the negative log likelihood
-#' }
 #' @keywords models
 #' @export
 
-halfnorm.like <- function(a, 
-                          dist, 
-                          covars = NULL, 
-                          w.lo = units::set_units(0,"m"), 
-                          w.hi = max(dist), 
-                          series = "cosine", 
-                          expansions = 0, 
-                          scale = TRUE, 
-                          pointSurvey = FALSE){
-  
-  # rule is: parameter 'a' never has units.  None of its components do, even though they could (e.g., sigma = a[1])
-  # upon entry: 'dist', 'w.lo', and 'w.hi' all have units 
-  dist[ (dist < w.lo) | (dist > w.hi) ] <- NA
-  
-  if(!is.null(covars)){
-    q <- ncol(covars)
-    beta <- a[1:q] 
-    s <- drop( covars %*% matrix(beta,ncol=1) )
-    sigma <- exp(s)
-  } else {
-    sigma <- a[1]
+halfnorm.like <- function(a
+                          , dist
+                          , covars ){
+
+  q <- ncol(covars)
+  beta <- a[1:q] # could be expansion coefs after q
+  s <- drop( covars %*% matrix(beta,ncol=1) )
+  sigma <- exp(s)  # link function here
+
+  if(inherits(dist, "units")){
+    dist <- units::drop_units(dist)
   }
+  key <- -(dist*dist)/(2*sigma*sigma)  
+  # Above is safe. Units of sigma will scale to units of dist. 
+  # 'key' is unit-less
+  key <- exp(key)  # exp of density function here, not link.
 
-  key <- -(units::drop_units(dist*dist))/(2*sigma*sigma)  
-  # Above is safe. Units of sigma will scale to units of dist. 'key' is unit-less
-  key <- exp(key)
-  
-  # If there are expansion terms
-  if(expansions > 0){
-    
-    nexp <- expansions
-    w <- w.hi - w.lo  # 'w' has units here, we want this so conversions below happen
-    
-    if (series=="cosine"){
-      dscl <- units::drop_units(dist/w)   # unit conversion here; drop units is safe
-      exp.term <- cosine.expansion( dscl, nexp )
-    } else if (series=="hermite"){
-      dscl <- units::drop_units(dist/sigma) # unit conversion here; drop units is safe
-      exp.term <- hermite.expansion( dscl, nexp )
-    } else if (series == "simple") {
-      dscl <- units::drop_units(dist/w)    # unit conversion here; drop units is safe
-      exp.term <- simple.expansion( dscl, nexp )
-    } else {
-      stop( paste( "Unknown expansion series", series ))
-    }
-    
-    expCoeffs <- a[(length(a)-(nexp-1)):(length(a))]
-    key <- key * (1 + c(exp.term %*% expCoeffs))
-    
-    # without monotonicity restraints, function can go negative, 
-    # especially in a gap between datapoints. This makes no sense in distance
-    # sampling and screws up the convergence. 
-    key[ which(key < 0) ] <- 0
-    
-  } 
-  
-  if( scale ){
-    key = key / integration.constant(dist=dist, 
-                                         density=halfnorm.like, 
-                                         a=a,
-                                         covars = covars, 
-                                         w.lo=w.lo, 
-                                         w.hi=w.hi, 
-                                         series=series, 
-                                         expansions=expansions, 
-                                         pointSurvey = pointSurvey)   # scales underlying density to integrate to 1.0
+  return( list(key = key, params = sigma))  
 
-  }
-
-  c(key)
 }
---
+++ R/nLL.R
@@ -0,0 +1,190 @@
+#' @title nLL - Negative log likelihood of distances
+#' 
+#' @description Return the negative log likelihood of 
+#' a vector of observed distances given a specified likelihood, 
+#'   number of expansion terms, and estimated parameters.
+#'
+#' @param a A vector of likelihood parameter values. Length and 
+#' meaning depend on \code{ml$series} and \code{ml$expansions}. If no expansion 
+#' terms were called for (i.e., \code{ml$expansions = 0}), the distance 
+#' likelihoods contain one or two canonical parameters (see Details). 
+#' If one or more expansions are called for, coefficients for the 
+#' expansion terms follow coefficients for the canonical parameters.  
+#' i.e., length of this vector must be 
+#'   \code{(num Covars incl. intercept) + expansions + 1*(like \%in\% c("hazrate", "logistic", "huber"))}.
+#'   
+#' @inheritParams startLimits
+#'  
+#' @details
+#' \bold{Expansion Terms}: If \code{ml$expansions} = k (k > 0), 
+#' the expansion function specified by \code{ml$series} is 
+#' called (see for example \code{\link{cosine.expansion}}). 
+#' Assuming \eqn{h_{ij}(x)}{h_ij(x)} is the 
+#' \eqn{j^{th}}{j-th} expansion term for the 
+#' \eqn{i^{th}}{i-th} distance and that 
+#' \eqn{c_1, c_2, \dots, c_k}{c(1), c(2), ..., c(k)} 
+#' are (estimated) coefficients for the expansion terms, 
+#' the likelihood contribution for the \eqn{i^{th}}{i-th} 
+#' distance is, \deqn{f(x|a,b,c_1,c_2,\dots,c_k) = 
+#' f(x|a,b)(1 + \sum_{j=1}^{k} c_j h_{ij}(x)).}{f(x|a,b,c_1,c_2,...,c_k) 
+#' = f(x|a,b)(1 + c(1) h_i1(x) + c(2) h_i2(x) + ... + c(k) h_ik(x)). }
+#'   
+#' @return A scalar, the negative of the log likelihood evaluated at 
+#' parameters \code{a}, including expansion terms.
+#' 
+#' @seealso See \code{\link{halfnorm.like}} and links there; 
+#'  \code{\link{dfuncEstim}}
+#' 
+#' @examples
+#' set.seed(238642)
+#' 
+#' # Close to what happens in Rdistance
+#' d <- rnorm(1000, mean = 0, sd = 40)
+#' d <- units::set_units(d[0 <= d], "m")
+#' ml <- list(
+#'     mf = model.frame(d ~ 1) 
+#'   , likelihood = "halfnorm"
+#'   , expansions = 0
+#'   , w.lo = units::set_units(0, "m")
+#'   , w.hi = units::set_units(125, "m")
+#' )
+#' nLL(log(40), ml)
+#' 
+#' # A direct way
+#' ones <- matrix(1, nrow = length(d), ncol = 1)
+#' l <- halfnorm.like(log(40), d, ones)
+#' scaler <-(pnorm(units::drop_units(ml$w.hi)
+#'   , units::drop_units(ml$w.lo)
+#'   , sd = l$params) - 0.5) * sqrt(2*pi) * l$params
+#' -sum(log(l$key/scaler))
+#'
+#' # A third direct way, b/c is it half normal and we 
+#' # have pnorm() and dnorm(). 
+#' l2 <- dnorm(units::drop_units(d), mean = 0, sd = 40)
+#' scaler2 <- pnorm(125, mean = 0, sd = 40) - 0.5 
+#' -sum(log(l2/scaler2))
+#'  
+#' @keywords models
+#' @export
+
+nLL <- function(a,
+                  ml){
+  
+  # Pull data from input list ----
+  # rule is: parameters in 'a' never have units,  
+  # even though they could (e.g., sigma of halfnorm)
+  #
+  # upon entry: 'dist', 'w.lo', and 'w.hi' all have units 
+  #
+  # could move these retrievals outside nLL, which might 
+  # speed things a little, but it's more params to pass in.
+  f.like <- match.fun(paste( ml$likelihood, ".like", sep=""))
+  dist <- stats::model.response(ml$mf)
+  X <- stats::model.matrix(stats::terms(ml$mf), ml$mf)
+    
+  # Because of na.exclude when building model frame, and 
+  # other code in parseModel(), there are no missing distances 
+  # in the response, nor are any outside the strip.  
+
+      
+  # Evaluate the "key" function ----
+  # I call Buckland's "key" function just "likelihood".
+  # Returns one value per observation. Expansions, if any, 
+  # are applied below, after this.
+  L <- f.like( a = a
+             , dist = dist
+             , covars = X
+             )
+  key <- L$key
+  
+  # Evaluate the expansions ----
+  # If there are any.
+  if( ml$expansions > 0 ){
+    
+    nexp <- ml$expansions
+    w <- ml$w.hi - ml$w.lo  # 'w' has units here, we want this so conversions below happen
+    
+    if (ml$series=="cosine"){
+      dscl <- units::drop_units(dist/w)   # unit conversion here; drop units is safe
+      exp.term <- cosine.expansion( dscl, nexp )
+    } else if (ml$series=="hermite"){
+      dscl <- units::drop_units(dist/sigma) # unit conversion here; drop units is safe
+      exp.term <- hermite.expansion( dscl, nexp )
+    } else if (ml$series == "simple") {
+      dscl <- units::drop_units(dist/w)    # unit conversion here; drop units is safe
+      exp.term <- simple.expansion( dscl, nexp )
+    } else {
+      stop( paste( "Unknown expansion series. Found", ml$series ))
+    }
+    
+    expCoeffs <- a[(length(a)-(nexp-1)):(length(a))]
+    key <- key * (1 + c(exp.term %*% expCoeffs))
+    
+    # without monotonicity restraints, function can go negative, 
+    # especially in a gap between datapoints. Don't want this in distance
+    # sampling and screws up the convergence. In future, could
+    # apply monotonicity constraint here.
+    key[ which(key < 0) ] <- 0
+    
+  } 
+  
+  # Scale the likelihood ----
+  # Scalers should be unique to each observation and equal
+  # to integral under distance function for that observation. 
+  # Integrals are by defn unit-less.
+  if( ml$expansions <= 0 && 
+      (ml$likelihood %in% c("halfnorm", "negexp"))){
+    # We know the integral in some cases.  
+    # To speed things up, evaluate the integrals we know.
+    theta <- L$params # n X (1) matrix of likelihood parameters in these cases
+    if( ml$likelihood == "halfnorm"){
+      # We evaluate normal with mean set to w.lo, sd = sigma, from -Inf to w.hi, then
+      # subtract 0.5 from result for the area to left of mean (w.lo) 
+      # theta = sigma in this case.
+      scaler <- (pnorm(units::drop_units(ml$w.hi)
+                       , units::drop_units(ml$w.lo)
+                       , theta) - 0.5) * 
+        sqrt(2*pi) * theta
+    } else if( ml$likelihood == "negexp" ){
+      # theta = beta (n X 1 vector) in this case
+      scaler <- unname(
+        (exp(-theta * units::drop_units(w.lo)) -
+           exp(-theta * units::drop_units(w.hi))) / theta)
+    } 
+    
+    key = key / scaler
+    
+  } else {
+    # We numerically integrate.  These are integrals we 
+    # do not know and any that have expansions
+    key = key / integration.constant(ml)
+    
+  }
+
+  
+  
+  if( !is.null(getOption("Rdistance_optimizer")) &&
+      (getOption("Rdistance_optimizer") == "optim") ){
+    key <- key*10^9 # optim likes big numbers
+  }
+  
+  #print(L)
+  key[ !is.na(key) & (key <= 0) ] <- getOption("Rdistance_zero")   # happens at very bad values of parameters
+
+  nLL <- -sum(log(key), na.rm=TRUE)  # Note that distances > w in L are set to NA
+
+  # Rules: 
+  #   RULE 1 FOR LIKELIHOODS: No matter how bad the guess at a, you cannot return Inf, -Inf, NA, or NaN
+  #   This means f.like can return NA, but not NaN (div by 0), Inf or -Inf for any row of data
+  #   Must program the likelihoods to trap these values and return the appropriate .Machine constants
+  #
+  #   RULE 2 FOR NLL: It is possible for all rows of returned L to be non-Inf and non-NaN, 
+  #   but there be enough of them that the sum overflows to Inf. I.e., values in L are 
+  #   hyper-close to 0 and log(L) is close to -Inf, then sum overflows.  Trap this here.
+  #
+  if( is.infinite(nLL) ){
+    nLL <- getOption("Rdistance_posInf") # positive b/c already flipped over by -1
+  }
+  
+  nLL
+}
