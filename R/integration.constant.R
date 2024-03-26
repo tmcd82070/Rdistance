8e76525f6c91bd4b388e19188695267f05ffb55d
Trent McDonald <trent@mcdonalddatasciences.com>
2024-03-26 10:11:50 -0600

    Complete revamp of integration.constant.R
    Before: integration constant was same as old version, i.e., lots of special cases.
    
    After: fewer special cases. Incorporated dplyr methods into computations.
    
    files commited:
      R/integration.constant.R

--- R/integration.constant.R
+++ R/integration.constant.R
@@ -4,61 +4,48 @@
 #' the area under a distance function between two limits (\code{w.lo}
 #' and \code{w.hi}).
 #'
-#' @param dist Vector of detection distance values.
-#'
-#' @param density A likelihood function for which the
-#' integration constant is sought. This function
-#' must be capable of evaluating values between \code{w.lo}
-#' and \code{w.hi} and have the following parameters:
-#'     \itemize{
-#'       \item \samp{a} = Parameter vector.
-#'       \item \samp{dist} = Vector of distances.
-#'       \item \samp{covars} = If the density allows covariates, 
-#'       the covariate matrix.
-#'       \item \samp{w.lo} = Lower limit or left truncation value.
-#'       \item \samp{w.hi} = Upper limit or right truncation value.
-#'       \item \samp{series} = Form of the series expansions, if any.
-#'       \item \samp{expansions} = Number of expansion terms.
-#'       \item \samp{scale} = Whether to scale function to integrate to 1.
-#'     }
-#'
-#' @param w.lo The lower limit of integration, or the left truncation
-#' value for perpendicular distances.
-#'
-#' @param w.hi The upper limit of integration, or the right truncation
-#' value for perpendicular distances.
-#'
-#' @param covars Matrix of covariate values.
-#'
-#' @param a Vector of parameters to pass to \code{density}.
-#'
-#' @param series The series to use for expansions. 
-#' If \code{expansions} > 0, this string 
-#' specifies the type of expansion. Valid values at 
-#' present are 'simple', 'hermite', and 'cosine'.
+#' @inheritParams nLL
 #' 
-#' @param expansions Number of expansions in \code{density}.
-#'
-#' @param pointSurvey Boolean. TRUE if point transect data,
-#' FALSE if line transect data.
-#'
+#' @inheritParams startLimits
+#' 
 #'
 #' @details The trapezoid rule is used to numerically integrate
-#' \code{density} from \code{w.lo} to \code{w.hi}. Two-hundred
-#' (200) equal-sized trapezoids are used in the integration.  The number
-#' of trapezoids to use is fixed and cannot be changed without
+#' \code{ml$likelihood} from \code{ml$w.lo} to \code{ml$w.hi}. 
+#' Two-hundred (200) equal-sized trapezoids are used in the 
+#' integration.  The number
+#' of trapezoids is fixed and cannot be changed without
 #' re-writing this routine.
 #'
-#' @return A scalar (or vector of scalars if covariates are present)
-#' that is the area under \code{density} between \code{w.lo} and \code{w.hi}.
-#' This scalar can be used as a divisor to scale density such that
-#' it integrates to 1.0. If x = density(\ldots), then
+#' @return A vector of scalars the same length as the 
+#' number of distance observations where each value
+#' is the area under \code{ml$likelihood} between 
+#' \code{w.lo} and \code{w.hi}.
+#' This scalar can be used (as a divisor) to scale 
+#' likelihood values such that
+#' it integrates to 1.0. i.e., if x = density(\ldots), then
 #' x / \code{integration.constant(density, \ldots)} will integrate to 1.0.
 #'
 #' @seealso \code{\link{dfuncEstim}}, \code{\link{halfnorm.like}}
 #'
 #' @examples
-#' # Can put any number for first argument (1 used here)
+#' set.seed(238642)
+#' 
+#' d <- rnorm(1000, mean = 0, sd = 40)
+#' d <- units::set_units(d[0 <= d], "m")
+#' obs <- factor(rbinom(length(d),1,0.5), labels = c("obs1", "obs2"))
+#' 
+#' # Close to what happens in Rdistance
+#' ml <- list(
+#'     mf = model.frame(d ~ obs) 
+#'   , likelihood = "halfnorm"
+#'   , expansions = 0
+#'   , w.lo = units::set_units(0, "m")
+#'   , w.hi = units::set_units(125, "m")
+#' )
+#' integration.constant(c(log(40), log(.5)), ml)
+
+
+# Can put any number for first argument (1 used here)
 #' scl <- integration.constant(dist=units::set_units(1,"m")
 #'                           , density=logistic.like
 #'                           , covars = NULL
@@ -78,18 +65,14 @@
 #' @importFrom stats integrate
 #' @export
 
-integration.constant <- function(dist, 
-                                 density, 
-                                 a,
-                                 covars, 
-                                 w.lo, 
-                                 w.hi, 
-                                 series,
-                                 expansions, 
-                                 pointSurvey){
+integrationConstant <- function(a, ml){
 
-  density = match.fun(density)
-  
+  fx <- match.fun(paste( ml$likelihood, ".like", sep=""))
+  dist <- stats::model.response(ml$mf)
+  X <- stats::model.matrix(stats::terms(ml$mf), ml$mf)
+  w.lo <- ml$w.lo
+  w.hi <- ml$w.hi
+
   # We need w.lo, w.hi, and dist to have same units. 
   # This is important because we occasionally drop units in integral calculations below. 
   # I cannot think of a case where units(w.lo) != units(dist), 
@@ -105,195 +88,127 @@ integration.constant <- function(dist,
   # This is done below, in each case where its needed. 
   
   nTrapazoids <- 200 # number of evaluation points in numerical integration
+
+  # For case is for LINES, COVARS, LIKE in {Logistic, User, Gamma} 
+  # and ALL LIKELIHOODS with expansions > 0
+  #
+  # We could do all likelihoods this way (i.e., numerical integration); but,
+  # the above special cases are faster (I think) and more accurate in some cases because
+  # we know the theoretical integral (i.e., for normal and exponential)
+  #
+  # Because we have covariates always, we need to integrate
+  # once per row of dist. i.e., each dist could be coming from 
+  # a separate distance function defined by separate parameters.
   
-  if(!is.null(covars)){
-    # Not sure following is best to do. 
-    # It is much faster to de-dup and compute values on just 
-    # unique combinations of covariates IF there are large number
-    # of duplicated covariates.  This is common when using 
-    # factors.  But, if using continuous covariates, this is 
-    # inefficient.  Regardless, this code only computes values 
-    # of the scaling constant for unique combinations of X, 
-    # then merges them into the covar array.  The non-de-dupped
-    # way to do these calculations is something like 
-    # x <- covars %*% matrix(a,ncol=1), which might be just as fast.
-    
-    Pkey <- tapply(1:nrow(covars), as.data.frame(covars)) # groups of duplicates
-    covars <- data.frame(covars, zzzPkey=Pkey)
-    dupCovars <- duplicated(covars$zzzPkey)
-    unique.covars <- covars[!dupCovars,] 
-    PkeyCol <- ncol(unique.covars)
-    
-    # Remember that unique.covars now has extra column, zzzPkey 
-    # don't include this column in calculations below (or set a[last]=0)
-    # covars and unique.covars now have Pkey, which we will use to 
-    # merge later
+  seqx = seq(w.lo, w.hi, length=nTrapazoids) 
 
-    seqy <- list()
-    temp.scaler <- vector(length = nrow(unique.covars))
-    scaler <- vector(length = nrow(covars), "numeric")
+  # Find covariate classes ----
+  # Rep the integration sequence over covariate rows.
+  # i.e., each x sequence will be associated with one set of 
+  # covariates from one row. This makes a big data frame.
+  uniqueX <- data.frame(X) |>
+    dplyr::mutate(.covClsRow = dplyr::row_number()) |> 
+    dplyr::distinct( dplyr::across(-.covClsRow)
+                     , .keep_all = TRUE )
 
-    if(pointSurvey){
-      # This case is POINTS, COVARS, all Likelihoods
-      seqx = seq(w.lo, w.hi, length=nTrapazoids) 
-      for(i in 1:nrow(unique.covars)){
-        temp.covars <- matrix(as.numeric(unique.covars[i,-PkeyCol])
-                            , nrow = length(seqx)
-                            , ncol = ncol(unique.covars)-1
-                            , byrow=TRUE)
-        seqy[[i]] <- units::drop_units(seqx) * density(a = a
-                                  , dist = seqx
-                                  , covars = temp.covars
-                                  , scale = FALSE
-                                  , w.lo = w.lo
-                                  , w.hi = w.hi
-                                  , expansions = expansions
-                                  , series=series
-                                  )
-        temp.scaler[i] <- units::drop_units(seqx[2] - seqx[1]) * sum(seqy[[i]][-length(seqy[[i]])] + seqy[[i]][-1]) / 2
-      }
+  covClsMap <- data.frame(X) |> 
+    dplyr::mutate(.rowNumber = dplyr::row_number()) |> 
+    dplyr::group_by(dplyr::across(-.rowNumber)) |> 
+    dplyr::mutate( .covClsRow = dplyr::first(.rowNumber)) |> 
+    dplyr::ungroup() |> 
+    dplyr::select(.rowNumber, .covClsRow)
+  
+  
+  # Internal function to evaluate key ----
+  # Eval fx for each set of constant covariates (rowid)
+  # This is a function to apply density to each row of covariates
+  # designed for dplyr::group_modify()
+  evalKey <- function(covs
+                      , .y
+                      , a
+                      , Seqx
+                      , pointSurvey
+                      ){
+    # covars are constant across all rows of Seqx
+    covs <- matrix(as.numeric(covs), nrow = 1)
+    seqy <- fx(
+        a = a
+      , dist = Seqx
+      , covars = covs
+    )$key
 
-    } else if(identical(density, halfnorm.like) & expansions == 0){
-      # this case is LINES, COVARS, HALFNORM, NO EXPANSIONS
-      # Made this a case because I think it's faster, because we know integral
-      s <- as.matrix(unique.covars) %*% matrix(c(a,0),ncol=1)
-      sigma <- exp(s)  # link function here
-
-      # temp.scaler should be integral under distance function
-      # We happen to know it for halfnorm (and some others below)
-      # Integrals are by defn unit-less; but, pnorm returns units. Drop apriori.
-      # We evaluate normal with mean w.lo, sd = sigma, from -Inf to w.hi, then
-      # subtract 0.5 from result for the area to left of mean (w.lo) 
-      temp.scaler <- (pnorm(units::drop_units(w.hi)
-                          , units::drop_units(w.lo)
-                          , sigma) - 0.5) * 
-                      sqrt(2*pi) * sigma
-      
-    } else if(identical(density, hazrate.like) & expansions == 0){
-      # This case is LINES, HAZRATE, COVARS, NO EXPANSIONS
-      #
-      # Integral of hazrate involves incomplete gamma functions. 
-      # See wolfram.  Incomplete gammas are implemented in some packages, e.g., 
-      # expint.  You could convert to an exact integral using one of these 
-      # packages.  But, for now, numerically integrate.
-      seqx = seq(w.lo, w.hi, length=nTrapazoids) 
-      beta <- a[-length(a)]
-      K <- a[length(a)]
-      s <- as.matrix(unique.covars[,-PkeyCol]) %*% matrix(beta,ncol=1)
-      sigma <- exp(s)  # link function here
-
-      temp.scaler <- sapply(sigma 
-                      , FUN = function(s, Seqx, KK){ 
-                        seqy <- 1 - exp(-(Seqx/s)^(-KK))
-                        scaler <- (Seqx[2] - Seqx[1])*sum(seqy[-length(seqy)] + seqy[-1]) / 2
-                        scaler }
-                      , Seqx = units::drop_units(seqx)
-                      , KK = K)
-    } else if(identical(density, negexp.like) & expansions == 0){
-      # This case is LINES, NEGEXP, COVARS, NO EXPANSIONS
-      s <- as.matrix(unique.covars) %*% matrix(c(a,0),ncol=1)
-      beta <- exp(s)
-      temp.scaler <- unname((exp(-beta * units::drop_units(w.lo)) - 
-                               exp(-beta * units::drop_units(w.hi)))/beta)
-    } else {
-      # For case is for LINES, COVARS, LIKE in {Logistic, User, Gamma} 
-      # and ALL LIKELIHOODS with expansions > 0
-      #
-      # We could do all likelihoods this way (i.e., numerical integration); but,
-      # the above special cases are faster (I think) and more accurate in some cases because
-      # we know the theoretical integral (i.e., for normal and exponential)
-      seqx = seq(w.lo, w.hi, length=nTrapazoids) 
-
-      # function to apply density to each row of covariates
-      likeApply <- function(covs
-                          , Seqx
-                          , W.lo 
-                          , W.hi 
-                          , A 
-                          , Expansions 
-                          , Series  
-                          ){
-        # Matrix of constant covariates for this case
-        temp.covars <- matrix(covs
-                              , nrow = length(Seqx)
-                              , ncol = length(covs)
-                              , byrow = TRUE
-        )
-        seqy <- density(dist = Seqx
-                             , covars = temp.covars
-                             , scale = FALSE
-                             , w.lo = W.lo
-                             , w.hi = W.hi
-                             , a = A
-                             , expansions = Expansions
-                             , series = Series 
-        )        
-        scaler <- units::drop_units(Seqx[2] - Seqx[1])*sum(seqy[-length(seqy)] + seqy[-1]) / 2
-        scaler
-      }
-     
-      temp.scaler <- apply(X = unique.covars[, -PkeyCol, drop = FALSE] 
-                         , MARGIN = 1
-                         , FUN = likeApply
-                         , Seqx = seqx
-                         , W.lo = w.lo 
-                         , W.hi = w.hi
-                         , A = a
-                         , Expansions = expansions 
-                         , Series = series
-                         )
-      
+    if(ml$expansions > 0){
+      # 'if' saves a little compute time if no expansions
+      # but not necc. b/c expansionTerms = 1 if none
+      key <- key * expansionTerms(a, mf)
     }
-
-    df <- data.frame(unique.covars, temp.scaler)
-
-    z <- merge(covars, df, by.x="zzzPkey", by.y="zzzPkey", sort=F)
-    scaler <- z$temp.scaler
+    
     if(pointSurvey){
-      scaler <- scaler/units::drop_units(dist)
+        key <- units::drop_units(Seqx) * key 
     }
-  } else if( pointSurvey ){
-    # This case is POINTS - NO Covariates
-    seqx = seq(w.lo, w.hi, length=nTrapazoids) 
-    seqy <- units::drop_units(seqx) * density( dist = seqx, scale = FALSE, 
-                            w.lo = w.lo, w.hi = w.hi, a = a, 
-                            expansions = expansions, series=series)
-
-    #   trapezoid rule
-    scaler <- units::drop_units(seqx[2]-seqx[1]) * sum(seqy[-length(seqy)]+seqy[-1]) / (2*units::drop_units(dist))
-  } else {
-    # This case is LINES - NO Covariates
-    # Density should return unit-less numbers (height of density function)
-    seqx = seq(w.lo, w.hi, length=nTrapazoids) 
-    seqy <- density( dist = seqx
-                   , scale = FALSE
-                   , w.lo = w.lo
-                   , w.hi = w.hi
-                   , a = a
-                   , expansions = expansions
-                   , series=series
-                   )
-
-    #   trapezoid rule
-    scaler <- units::drop_units(seqx[2]-seqx[1]) * sum(seqy[-length(seqy)]+seqy[-1]) / 2
+      
+    scaler <- units::drop_units(Seqx[2] - Seqx[1]) * 
+      sum(seqy[-length(seqy)] + seqy[-1]) / 2
+    
+    data.frame(scaler = scaler)
   }
   
+  # Call internal evalKey function using group_modify ----
+  scalers <- uniqueX |> 
+    dplyr::group_by(.covClsRow) |> 
+    dplyr::group_modify(.f = evalKey
+                        , a = a
+                        , Seqx = seqx)
+  
+  # This join expands from unique covariate classes
+  # to the original size of covars
+  scalers <- covClsMap |> 
+    dplyr::left_join(scalers, by = ".covClsRow") |> 
+    dplyr::arrange(.rowNumber) |>   # probably not necc. to sort
+    dplyr::pull(scaler)
+
+   # } else if( pointSurvey ){
+  #   # This case is POINTS - NO Covariates
+  #   seqx = seq(w.lo, w.hi, length=nTrapazoids) 
+  #   seqy <- units::drop_units(seqx) * density( dist = seqx, scale = FALSE, 
+  #                           w.lo = w.lo, w.hi = w.hi, a = a, 
+  #                           expansions = expansions, series=series)
+  # 
+  #   #   trapezoid rule
+  #   scaler <- units::drop_units(seqx[2]-seqx[1]) * sum(seqy[-length(seqy)]+seqy[-1]) / (2*units::drop_units(dist))
+  # } else {
+  #   # This case is LINES - NO Covariates
+  #   # Density should return unit-less numbers (height of density function)
+  #   seqx = seq(w.lo, w.hi, length=nTrapazoids) 
+  #   seqy <- density( dist = seqx
+  #                  , scale = FALSE
+  #                  , w.lo = w.lo
+  #                  , w.hi = w.hi
+  #                  , a = a
+  #                  , expansions = expansions
+  #                  , series=series
+  #                  )
+  # 
+  #   #   trapezoid rule
+  #   scaler <- units::drop_units(seqx[2]-seqx[1]) * sum(seqy[-length(seqy)]+seqy[-1]) / 2
+  # }
+  # 
   # there are cases where the guess at parameters is so bad, that the integration
   # constant is 0 (consider pnorm(100,0,2e30)). But, we don't want to return 0
-  # because it goes in denominator of likelihood and results in Inf, which is 
-  # not informative.  nlminb guesses NaN after that sometimes. We want to return 
+  # because it goes in denominator of likelihood and results in Inf, which is
+  # not informative.  nlminb guesses NaN after that sometimes. We want to return
   # the smallest possible number that does not result in log(x) = -Inf.
-  # Because of the negative applied in nLL function we actually mant to return
+  # Because of the negative applied in nLL function we actually want to return
   # the largest possible numbers such that when we sum them and others we don't get Inf
 
-  if( any(indZeros <- is.na(scaler) | 
-               is.infinite(scaler) | 
-               is.nan(scaler) |
-               (scaler <= .Machine$double.xmin)) ){
-    scaler[ indZeros ] <- .Machine$double.xmax / sum(indZeros)
+  if( any(indZeros <- is.na(scalers) |
+               is.infinite(scalers) |
+               is.nan(scalers) |
+               (scalers <= getOption("Rdistance_fuzz"))) ){
+    scalers[ indZeros ] <- getOption("Rdistance_posInf")
   }
   
-  # cat(paste("\tscaler = \n\t", paste(scaler, collapse = ", "), "\n"))
-  
-  scaler
+  # # cat(paste("\tscaler = \n\t", paste(scaler, collapse = ", "), "\n"))
+  # 
+  scalers
 }
