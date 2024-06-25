d7bf4b2595750ceb13fdde66bb0666dc67c4606b
Trent McDonald <trent@mcdonalddatasciences.com>
2024-06-25 13:00:23 -0600

    Overhall of predict method for new code
    Only 'line' method is working.

--- R/predict.dfunc.R
+++ R/predict.dfunc.R
@@ -4,7 +4,7 @@
 #' actual distance functions from estimated distance function 
 #' objects.
 #' 
-#' @param object An estimated detection function, normally 
+#' @param x An estimated detection function, normally 
 #' produced by calling \code{\link{dfuncEstim}}. 
 #' 
 #' @param newdata A data frame containing new values of 
@@ -25,7 +25,7 @@
 #'   \item \bold{If \code{type} = "parameters"}: Returned values are
 #'     predicted (canonical) parameters of the likelihood function. 
 #'     If \code{newdata} is NULL, return contains one parameter
-#'     value for every detection in \code{object} with a distance.
+#'     value for every detection in \code{x} with a distance.
 #'     If \code{newdata} is not NULL, returned vector has one parameter
 #'     for every row in \code{newdata}.
 #'   \item \bold{If \code{type} is not "parameters"}: Returned  
@@ -33,11 +33,11 @@
 #'     one distance function per row. Distance functions 
 #'     are evaluated at distances   
 #'     specified in argument \code{distances}, not at the observed 
-#'     distances in \code{object}. The number of distance functions
+#'     distances in \code{x}. The number of distance functions
 #'     returned depends on \code{newdata}: 
 #'     \itemize{
 #'        \item If \code{newdata} is NULL, one distance function 
-#'        will be returned for every detected target in \code{object}
+#'        will be returned for every detected target in \code{x}
 #'        that has valid covariate values. 
 
 #'       \item If \code{newdata} is not NULL, one distance function 
@@ -45,19 +45,19 @@
 #'     }
 #'  }
 #'  
-#'  If \code{object} is a smoothed distance function, it does not have parameters
+#'  If \code{x} is a smoothed distance function, it does not have parameters
 #'  and this routine will always return a scaled distance function. That is, 
-#'  \code{type} = "parameters" when \code{object} is smoothed 
+#'  \code{type} = "parameters" when \code{x} is smoothed 
 #'  does not make sense and the smoothed distance function estimate will be returned. 
 #' 
 #' @param distances A vector of distances at which to evaluate 
 #' distance functions, when distance functions 
 #' are requested.  \code{distances} must have measurement units. 
 #' Any distances outside the observation 
-#' strip (\code{object$w.lo} to \code{object$w.hi}) are discarded.  If 
+#' strip (\code{x$w.lo} to \code{x$w.hi}) are discarded.  If 
 #' \code{distances} is NULL, this routine uses a sequence of 201 evenly 
 #' spaced distances between 
-#' \code{object$w.lo} and \code{object$w.hi}, inclusive. 
+#' \code{x$w.lo} and \code{x$w.hi}, inclusive. 
 #'
 #' @param \dots Included for compatibility with generic \code{predict} methods.
 #' 
@@ -67,7 +67,7 @@
 #'   contains predicted likelihood parameters. The extent of the 
 #'   first dimension (rows) in 
 #'   the returned matrix is equal to either the number of detection distances 
-#'   in \code{object} 
+#'   in \code{x} 
 #'   or number of rows in \code{newdata}. 
 #'   The returned matrix's second dimension (columns) is 
 #'   the number of parameters in the likelihood 
@@ -89,7 +89,7 @@
 #'        \code{newdata} is specified.
 #'     }
 #'   All distance functions in columns of the return are scaled 
-#'   to \code{object$g.x.scale} at \code{object$x.scl}.
+#'   to \code{x$g.x.scale} at \code{x$x.scl}.
 #'   
 #'   When \code{type} is not "parameters", the returned matrix has 
 #'   additional attributes. 
@@ -100,7 +100,7 @@
 #'   corresponding to each 
 #'   distance function in \code{return}. i.e., the vector of 
 #'   \code{1/f(x.scl)} where \code{f()} is the un-scaled distance function. 
-#'   If \code{object} contains line transects, \code{attr(return, "scaler")}
+#'   If \code{x} contains line transects, \code{attr(return, "scaler")}
 #'   is the vector of ESW corresponding to each distance function.
 #' }
 #' 
@@ -143,21 +143,21 @@
 #' 
 #' @importFrom stats terms as.formula delete.response model.frame model.matrix coef
 #' 
-predict.dfunc <- function(object
+predict.dfunc <- function(x
                         , newdata = NULL
                         , type = c("parameters")
                         , distances = NULL
                         , ...) {
 
-  if (!inherits(object, "dfunc")){ 
-    stop("Object is not a 'dfunc' object")
+  if (!inherits(x, "dfunc")){ 
+    stop("Argument 'x' is not a 'dfunc' object")
   }
   
-  isSmooth <- Rdistance::is.smoothed(object)
+  isSmooth <- Rdistance::is.smoothed(x)
   
 
   # if (missing(newdata) || is.null(newdata)) {
-  #   n <- nrow(object$mf)
+  #   n <- nrow(x$mf)
   # } else {
   #   n <- nrow(newdata)
   # }
@@ -166,12 +166,12 @@ predict.dfunc <- function(object
   # We ALWAYS have covariates
   # We always need parameters, except for smoothed dfuncs
   if( !isSmooth ){
-    if (missing(newdata) || is.null(newdata)) {
+    if ( is.null(newdata) ) {
       # Case: Use original covars
-      X <- model.matrix(object)
+      X <- model.matrix(x)
     } else {
       # Pull formula to make covars from NEWDATA
-      Terms <- terms( object$mf )
+      Terms <- terms( x$mf )
       Terms <- delete.response( Terms ) # model.frame (below) fails if there's a response, go figure.
       if( !is.null(attr(Terms, "offset")) ){
         # gotta add a fake groupsize to newdata so model.frame (below) works
@@ -180,12 +180,12 @@ predict.dfunc <- function(object
         newdata <- cbind(newdata, data.frame( 1 ))
         names(newdata)[length(names(newdata))] <- gsName
       }
-      xLevs <- lapply( object$mf, levels ) # get unspecified levels of factors
+      xLevs <- lapply( x$mf, levels ) # get unspecified levels of factors
       m <- model.frame( Terms, newdata, xlev = xLevs )
-      X <- model.matrix( Terms, m, contrasts.arg = attr(object$mf,"contrasts") )
+      X <- model.matrix( Terms, m, contrasts.arg = attr(x$mf,"contrasts") )
     }
     
-    BETA <- coef(object)
+    BETA <- coef(x)
     beta <- BETA[1:ncol(X)]   # could be extra parameters tacked on. e.g., knee for logistc or expansion terms
     paramsLink <- X %*% beta
     params <- exp(paramsLink)  # All link functions are exp...thus far
@@ -206,8 +206,8 @@ predict.dfunc <- function(object
   # DISTANCE function prediction ----
   
   if( is.null(distances) ){
-    distances <- seq( object$w.lo
-                    , object$w.hi
+    distances <- seq( x$w.lo
+                    , x$w.hi
                     , length = getOption("Rdistance_intEvalPts"))
   } else {
     # check distances have units
@@ -215,7 +215,7 @@ predict.dfunc <- function(object
       stop("Distances must have measurement units.")
     }
     # Make sure input distances are converted properly b.c. likelihoods drop units.
-    distances <- units::set_units(distances, object$outputUnits, mode = "standard")
+    distances <- units::set_units(distances, x$outputUnits, mode = "standard")
   }
   
   
@@ -223,27 +223,27 @@ predict.dfunc <- function(object
     # unscaled distance function is already stored
     # no covariates.  
     # NEED TO REVISIT THIS
-    y <- stats::approx( object$fit$x, object$fit$y, xout = distances )$y
+    y <- stats::approx( x$fit$x, x$fit$y, xout = distances )$y
     y <- matrix( y, nrow = 1) 
   } else {
     # After next apply, y is length(distances) x nrow(parms).  
     # Each row is a un-scaled distance function (f(x))
     # We already eval-ed covars, so new X is constant (1), "XIntOnly"
     
-    like <- match.fun( paste( object$likelihood, ".like", sep=""))
+    like <- match.fun( paste( x$likelihood, ".like", sep=""))
     XIntOnly <- matrix(1, nrow = length(distances), ncol = 1)
     y <- lapply(X = paramsLink
                , FUN = like
-               , dist = distances - object$w.lo
+               , dist = distances - x$w.lo
                , covars = XIntOnly
     )  
     y <- lapply(y, function(x){x$L.unscaled})
     y <- do.call(rbind, y)  
     
-    if(object$expansions > 0){
+    if(x$expansions > 0){
       # need null model with new responses 
       ml <- model.frame( distances ~ 1 )
-      obj <- object
+      obj <- x
       obj$mf <- ml
       exp.terms <- Rdistance::expansionTerms(BETA, obj)
       y <- t( t(y) * exp.terms ) 
@@ -254,7 +254,7 @@ predict.dfunc <- function(object
   
   # SCALE distance functions ----
     
-  if( object$likelihood == "Gamma" & !isSmooth ){
+  if( x$likelihood == "Gamma" & !isSmooth ){
     # This is a pesky special case of scaling. We need different x.scl for 
     # every distance function b.c. only x.scl = max is allowed.  For all other 
     # distance functions there is only one x.scl.
@@ -278,67 +278,73 @@ predict.dfunc <- function(object
     x0 <- apply(X = X
                      , MARGIN = 1
                      , FUN = maximize.g.reparam
-                     , fit = object
+                     , fit = x
                 )
     # x0 is a distance, needs units
-    x0 <- units::set_units(x0, object$outputUnits, mode = "standard")
+    x0 <- units::set_units(x0, x$outputUnits, mode = "standard")
     
   } else if( !isSmooth ){
     # Case:  All likelihoods except Gamma
-    x0 <- rep(object$x.scl, nrow(params))
+    x0 <- rep(x$x.scl, nrow(params))
   } 
   
   # Now that we know x0 (either a scaler or a vector), compute f(x0)
+  
   if( !isSmooth ){
-    likeAtX0 <- function(i, params, x0, like, fit ){
-      fx0 <- like(
-          a = params[i,]
-        , dist = x0[i] - fit$w.lo
-        , covars = matrix(1,nrow = 1,ncol = 1)
-      ) 
-      fx0$L.unscaled
-    }
-    
-    f.at.x0 <- sapply(1:nrow(params)
-                      , FUN = likeAtX0
-                      , params = params
-                      , x0 = x0
-                      , like = like
-                      , fit = object
-                )
-
-    if(object$expansions > 0){
-      # need null model with new responses 
-      ml <- model.frame( x0 ~ 1 )
-      obj <- object
-      obj$mf <- ml
-      exp.terms <- Rdistance::expansionTerms(BETA, obj)
-      f.at.x0 <- f.at.x0 * exp.terms
-    }
+  
+    # Likelihood functions return g(x). This is what we want.
+    # likeAtX0 <- function(i, params, x0, like, fit ){
+    #   fx0 <- like(
+    #       a = params[i,]
+    #     , dist = x0[i] - fit$w.lo
+    #     , covars = matrix(1,nrow = 1,ncol = 1)
+    #   ) 
+    #   fx0$L.unscaled
+    # }
+    # 
+    # f.at.x0 <- sapply(1:nrow(params)
+    #                   , FUN = likeAtX0
+    #                   , params = params
+    #                   , x0 = x0
+    #                   , like = like
+    #                   , fit = x
+    #             )
+    # 
+    # if(x$expansions > 0){
+    #   # need null model with new responses 
+    #   ml <- model.frame( x0 ~ 1 )
+    #   obj <- x
+    #   obj$mf <- ml
+    #   exp.terms <- Rdistance::expansionTerms(BETA, obj)
+    #   f.at.x0 <- f.at.x0 * exp.terms
+    # }
     
 
   } else {
     # Case: smoothed distance function.  No covars. 
     # This is so simple we handle everything here
-    x0 <- object$x.scl
+    # I THINK WE JUST NEED TO INTEGRATE UNDER Y, SO I DON'T THINK 
+    # WE NEED THIS SPECIAL CASE, BUT CHECK.
+    x0 <- x$x.scl
     f.at.x0 <- stats::approx(distances, y, xout = x0)$y 
   }
     
 
-  scaler <- object$g.x.scl / f.at.x0 # a length n vector, n = nrow(params) 
+  # scaler <- x$g.x.scl * esw # esw = a length n vector, n = nrow(params) 
 
   # Did you know that 'scaler' is ESW?  At least for lines. Makes sense. 1/f(0) = ESW in 
   # the old formulas.
   # For smoothed distance functions, we extended the range of dist by approx 2, and scaler
   # is twice too big. i.e., esw for smu's is approx scaler / 2.
   
-  y <- y * scaler  # length(scalar) == nrow(y), so this works right
+  # y <- y * scaler  # length(scalar) == nrow(y), so this works right
   
   attr(y, "x0") <- x0
-  if(isSmooth){
-    scaler <- scaler / 2
-  }
-  attr(y, "scaler") <- scaler
+  attr(y, "g.x.scl") <- x$g.x.scl
+  # if(isSmooth){
+  #   scaler <- scaler / 2
+  # }
+  # attr(y, "scaler") <- scaler
 
   
   return( y )  
