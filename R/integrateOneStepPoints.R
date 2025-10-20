abb93429049bb18c8b0d24592e61a93fdd0abbf1
Trent <trent@mcdonalddatasceinces.com>
2025-10-20 10:38:06 -0600

    Bug fix: correct integrateOneStepPoints

--- R/integrateOneStepPoints.R
+++ R/integrateOneStepPoints.R
@@ -14,19 +14,29 @@
 #' \code{nrow(newdata)}.  If \code{newdata} is NULL, 
 #' return length is \code{length(distances(object))}. 
 #' 
-#' @seealso [integrateNumeric()]; [integrateNegexp()]; 
-#' [integrateOneStep()] 
+#' @seealso \code{\link{integrateNumeric}}; \code{\link{integrateNegexp}}; 
+#' \code{\link{integrateOneStep}} 
 #' 
 #' @examples
 #' 
-#' # Check:
-#' w.hi <- 125
-#' w.lo <- 0
-#' s1 <- 40
-#' s2 <- exp(log(s1) + log(0.5))
-#' obs1Scaler <- (pnorm(w.hi, mean=w.lo, sd = s1) - 0.5) * sqrt(2*pi)*s1
-#' obs2Scaler <- (pnorm(w.hi, mean=w.lo, sd = s2) - 0.5) * sqrt(2*pi)*s2
-#' c(obs1Scaler, obs2Scaler)
+#' fit <- dfuncEstim(thrasherDf, dist~1, likelihood = "oneStep")
+#' integrateOneStepPoints(fit, newdata = data.frame(`(Intercept)`=1))
+#' EDR(fit, newdata = data.frame(`(Intercept)`=1))
+#' 
+#' # Check: 
+#' Theta <- exp(fit$par[1])
+#' Theta <- units::set_units(Theta, "m")
+#' p <- fit$par[2]
+#' w.hi <- fit$w.hi
+#' w.lo <- fit$w.lo
+#' g.at0 <- w.lo
+#' g.atT <- Theta
+#' g.atTPlusFuzz <- (((1-p) * Theta) / ((w.hi - Theta) * p))*Theta
+#' g.atWhi <- (((1-p) * Theta) / ((w.hi - Theta) * p))*w.hi
+#' area.0.to.T <- (Theta - w.lo) * (g.atT - g.at0) / 2 # triangle; Theta^2/2
+#' area.T.to.w <- (w.hi - Theta) * (g.atTPlusFuzz + g.atWhi) / 2 # trapazoid
+#' area <- area.0.to.T + area.T.to.w
+#' edr <- sqrt( 2*area )
 #' 
 #' @export
 #' 
@@ -39,45 +49,20 @@ integrateOneStepPoints <- function(object
                       , type = "parameters"
   )
   
-  Theta <- y[,1]
+  Theta <- units::set_units(y[,1], object$outputUnits, mode = "standard")
+  p <- y[,2]
+  w.hi <- object$w.hi # has units
+  fatT <- (((1-p) * Theta) / ((w.hi - Theta) * p)) # height of f from Theta to w.hi
 
-  w.hi <- units::set_units(object$w.hi, NULL)
-  
-  x <- c(cbind(0
-             , Theta
-             , Theta + 100*getOption("Rdistance_fuzz")
-             ,  w.hi))  # matrix in vector form
-  x <- units::set_units(x, object$outputUnits, mode = "standard")
-  
-  y <- stats::predict(object = object
-                      , newdata = newdata
-                      , type = "dfuncs"
-                      , distances = x
-  )
-  
-  # rows are parameters, and pages are parameters
-  x <- array( c(x), dim = c(nrow(newdata), 4, nrow(newdata)))
-  y <- array( c(y), dim = c(nrow(newdata), 4, nrow(newdata)))
-  
-  xy <- x*y
-  
   # Triangle between 0 and Theta
-  part1 <- 0.5 * xy[1,2,] # should be length(nrow(newdata))
-  
+  part1 <- Theta * Theta / 2
   # Trapazoid between Theta and w.hi
-  gAtT <- xy[1,3,]
-  gAtw <- xy[1,4,]
-  part2a <- (gAtw - gAtT) * (w.hi - Theta) / 2 # triangle
-  part2b <- gAtT * (w.hi - Theta) # base
+  gAtT <- fatT * Theta
+  gAtw <- fatT * w.hi
+  part2 <- (gAtw + gAtT) * (w.hi - Theta) / 2 
   
-  outArea <- part1 + part2a + part2b
+  outArea <- part1 + part2
   
-  outArea <- sqrt( 2 * outArea)
-  
-  outArea <- units::set_units(outArea
-                              , object$outputUnits
-                              , mode = "standard")
-  
-  outArea 
+  outArea # units are object$outputUnits^2
   
 }
